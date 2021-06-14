e4e2d6ce3fd14adb42a7ad123867ba70be0f4e1d
Oren Sokolowsky <oren@daostack.io>
2021-06-14 17:46:51 +0300

    temporary fix calculateApy function..

--- src/utils.ts
+++ src/utils.ts
@@ -170,6 +170,9 @@ export const getTokenMarketCap = async (tokenAddress: string) => {
 export const calculateApy = async (vault: IVault, hatsPrice: number) => {
   // TODO: Should be staking token - e.g. vault.stakingToken
   // TODO: If the divdier is 0 so we get NaN and then it shows "-". Need to decide if it's okay or show 0 in this case.
+  if (Number(fromWei(vault.totalStaking)) === 0 ) {
+    return undefined
+  }
   return Number(fromWei(vault.totalRewardPaid)) * Number(hatsPrice) / Number(fromWei(vault.totalStaking)) * await getTokenPrice("0x543Ff227F64Aa17eA132Bf9886cAb5DB55DCAddf");
 }
 
