1e65c9cada10b58a8bce900d4cd57e07e9884724
orenyodfat <orenyodfat@users.noreply.github.com>
2021-06-14 18:54:10 +0300

    temporary fix calculateApy function.. (#27)
    * temporary fix calculateApy function..
    
    * return 0 if totalstake is 0

--- src/utils.ts
+++ src/utils.ts
@@ -170,6 +170,9 @@ export const getTokenMarketCap = async (tokenAddress: string) => {
 export const calculateApy = async (vault: IVault, hatsPrice: number) => {
   // TODO: Should be staking token - e.g. vault.stakingToken
   // TODO: If the divdier is 0 so we get NaN and then it shows "-". Need to decide if it's okay or show 0 in this case.
+  if (Number(fromWei(vault.totalStaking)) === 0 ) {
+    return 0;
+  }
   return Number(fromWei(vault.totalRewardPaid)) * Number(hatsPrice) / Number(fromWei(vault.totalStaking)) * await getTokenPrice("0x543Ff227F64Aa17eA132Bf9886cAb5DB55DCAddf");
 }
 
@@ -220,5 +223,5 @@ export const isWithdrawSafetyPeriod = async (withdrawPeriod: string, safetyPerio
   if (currentBlockNumber) {
     return currentBlockNumber % (Number(withdrawPeriod) + Number(safetyPeriod)) >= Number(withdrawPeriod);
   }
-  return true; 
+  return true;
 }
