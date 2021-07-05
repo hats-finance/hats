3f767d6dba77c65f248525e7d9ac7dbd07783c63
Roie Natan <roie.natan@gmail.com>
2021-07-05 23:41:22 +0100

    .

--- src/settings.ts
+++ src/settings.ts
@@ -3,7 +3,3 @@ import { Networks } from "./constants/constants";
 export const NETWORK: Networks = process.env.NETWORK as Networks ?? Networks.rinkeby;
 export const SUBGRAPH_URI = process.env.SUBGRAPH_URI ?? "https://api.thegraph.com/subgraphs/name/hats-finance/hats_rinkeby_v2";
 export const DATA_POLLING_INTERVAL = 10000; // TODO: should be as env var?
-
-
-//"https://api.thegraph.com/subgraphs/name/hats-finance/hats"
-//Networks.main
