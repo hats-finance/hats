db74dbabc702d01a1ff1859caffaed9cbb6e3330
Oren Sokolowsky <oren@daostack.io>
2021-07-05 14:40:09 +0300

    update discord link

--- src/components/Gov.tsx
+++ src/components/Gov.tsx
@@ -4,7 +4,7 @@ import "../styles/Gov.scss";
 export default function Gov() {
   return (
     <div className="content gov-wrapper">
-      <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/3kHJFDUs">
+      <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/xDphwRGyW7">
         Forum in Discord
         <DiscordIcon />
       </a>
