959147c2ec57739cd295480a76e923a66e1ca7bd
Roie Natan <roie.natan@gmail.com>
2021-07-06 07:53:48 +0100

    .

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,9 @@
 import { Networks } from "./constants/constants";
 require('dotenv').config();
 
-export const NETWORK: Networks = process.env.NETWORK as Networks || Networks.rinkeby;
-export const SUBGRAPH_URI = process.env.SUBGRAPH_URI || "https://api.thegraph.com/subgraphs/name/hats-finance/hats_rinkeby_v2";
+console.log(process.env.REACT_APP_NETWORK);
+console.log(process.env.REACT_APP_SUBGRAPH_URI);
+
+export const NETWORK: Networks = process.env.REACT_APP_NETWORK as Networks || Networks.rinkeby;
+export const SUBGRAPH_URI = process.env.REACT_APP_SUBGRAPH_URI || "https://api.thegraph.com/subgraphs/name/hats-finance/hats_rinkeby_v2";
 export const DATA_POLLING_INTERVAL = 10000; // TODO: should be as env var?
