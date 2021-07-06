87c2259188611f6ec0cb0bcdc8749f355fe29c12
Roie Natan <roie.natan@gmail.com>
2021-07-06 08:03:51 +0100

    some nft paddings fix

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,6 @@
 import { Networks } from "./constants/constants";
 require('dotenv').config();
 
-console.log(process.env.REACT_APP_NETWORK);
-console.log(process.env.REACT_APP_SUBGRAPH_URI);
-
 export const NETWORK: Networks = process.env.REACT_APP_NETWORK as Networks || Networks.rinkeby;
 export const SUBGRAPH_URI = process.env.REACT_APP_SUBGRAPH_URI || "https://api.thegraph.com/subgraphs/name/hats-finance/hats_rinkeby_v2";
 export const DATA_POLLING_INTERVAL = 10000; // TODO: should be as env var?
