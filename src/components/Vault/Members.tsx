8909169eef127dd988ab3093dd4899bccce1abd6
Roie Natan <roie.natan@gmail.com>
2021-08-30 17:21:03 +0100

    temp fix for issue in committe json

--- src/components/Vault/Members.tsx
+++ src/components/Vault/Members.tsx
@@ -7,10 +7,19 @@ interface IProps {
 }
 
 export default function Members(props: IProps) {
+
+  // TODO: temp fix
+  const verifyIPFSLink = (memberLink: string) => {
+    if (memberLink.startsWith("ipfs/")) {
+      return memberLink.slice(5);
+    }
+    return memberLink;
+  }
+
   const members = props.members?.map((member: ICommitteeMember, index: number) => {
     return (
       <a className="member-link-wrapper" key={index} href={member?.["twitter-link"]} target="_blank" rel="noreferrer">
-        {member?.["image-ipfs-link"] ? <img src={`${IPFS_PREFIX}${member?.["image-ipfs-link"]}`} alt="twitter avatar" className="twitter-avatar" /> : <TwitterImageIcon />}
+        {member?.["image-ipfs-link"] ? <img src={`${IPFS_PREFIX}${verifyIPFSLink(member?.["image-ipfs-link"])}`} alt="twitter avatar" className="twitter-avatar" /> : <TwitterImageIcon />}
         <span className="member-username">{member.name}</span>
       </a>
     )
