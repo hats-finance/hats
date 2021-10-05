cdc148535ffd7fb8bc18a3524d4d116ec35f3956
Roie Natan <roie.natan@gmail.com>
2021-10-05 16:57:55 +0400

    update to the LiquidityPools.tsx

--- src/components/LiquidityPools/LiquidityPools.tsx
+++ src/components/LiquidityPools/LiquidityPools.tsx
@@ -13,7 +13,7 @@ import { useEffect, useState } from "react";
 export default function LiquidityPools() {
   const rewardsToken = useSelector((state: RootState) => state.dataReducer.rewardsToken);
   const { loading, error, data } = useQuery(getIncentives(rewardsToken, false), { pollInterval: DATA_POLLING_INTERVAL, context: { clientName: LP_UNISWAP_V3_HAT_ETH_APOLLO_CONTEXT } });
-  const [incentives, setIncentives] = useState([]);
+  const [incentives, setIncentives] = useState<IIncentive[]>([]);
 
   useEffect(() => {
     if (!loading && !error && data && data.incentives) {
@@ -21,10 +21,10 @@ export default function LiquidityPools() {
     }
   }, [loading, error, data])
 
-  // TODO: temporary - show only this incentive
+  // TODO: note we assume for now we get one incentive that matches our query so we take the first in position [0]
   return (
     <div className="content liquidity-pools-wrapper">
-      {incentives.length === 0 ? <Loading fixed /> : <LiquidityPool incentive={incentives.filter((incentive: IIncentive)=> incentive.id === "0x96c4ed92424f1682883fef9ed9e6e7dc0bc1c1f939b946ea800d689961b6bc3f")[0]} />}
+      {loading ? <Loading fixed /> : incentives.length === 0 ? "No Incentives" : <LiquidityPool incentive={incentives[0]} />}
     </div>
   )
 }
