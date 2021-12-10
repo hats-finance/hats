d865ed8d1d0d1f2ad53bfc3fb228434b2ccbbb92
Roie Natan <roie.natan@gmail.com>
2021-12-10 17:16:17 -0500

    use INFURA_ID as env var

--- src/settings.ts
+++ src/settings.ts
@@ -4,5 +4,8 @@ require('dotenv').config();
 export const NETWORK: Networks = process.env.REACT_APP_NETWORK as Networks || Networks.rinkeby;
 export const SUBGRAPH_URI = process.env.REACT_APP_SUBGRAPH_URI || DEFAULT_RINKEBY_SUBGRAPH_URI;
 export const LP_UNISWAP_URI = process.env.REACT_APP_LP_UNISWAP_URI || LPUniswapURIs[NETWORK];
-export const MASTER_ADDRESS = process.env.REACT_APP_MASTER_ADDRESS || MasterAddresses[NETWORK]
+export const MASTER_ADDRESS = process.env.REACT_APP_MASTER_ADDRESS || MasterAddresses[NETWORK];
 export const DATA_POLLING_INTERVAL = 10000;
+
+/** Currently not in use */
+export const INFURA_ID = process.env.REACT_APP_INFURA_ID || "";
