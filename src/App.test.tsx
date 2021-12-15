ed0ac63535c9435702f9912155412cdac2362a2b
Richard Mands <richard@devwell.org>
2021-12-15 16:49:25 +0000

    Test Localization
    - test localization to jp
    - added localStorage test helper

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,17 +2,27 @@ import React from "react";
 import {
   renderConnected,
   within,
+  cleanup,
   fireEvent
 } from "../test/@testing-library/react";
+import { setLocalStorage } from "../test/testHelpers";
 
 import App from "./App";
 
-const defaultProps = {};
+const defaultLocalStorage = { i18nextLng: "en" };
+
+beforeEach(() => {
+  jest.resetModules();
+  setLocalStorage(defaultLocalStorage);
+});
+
+afterEach(() => {
+  cleanup();
+  jest.restoreAllMocks();
+});
 
 test("Loads the App to the Welcome screen and accept cookies", async () => {
-  const { getByTestId, queryByTestId } = renderConnected(
-    <App {...defaultProps} />
-  );
+  const { getByTestId, queryByTestId } = renderConnected(<App />);
 
   // Welcome
   expect(within(getByTestId("Welcome")).getByText("Hats")).toBeInTheDocument();
@@ -47,4 +57,13 @@ test("Loads the App to the Welcome screen and accept cookies", async () => {
   expect(
     within(getByTestId("Header")).getByText("Connect a Wallet")
   ).toBeInTheDocument();
+});
+
+test("Loads the App to the Welcome screen with localized text", () => {
+  setLocalStorage({ ...defaultLocalStorage, i18nextLng: "jp" });
+  const { getByTestId } = renderConnected(<App />);
+
+  const welcome = getByTestId("Welcome");
+  expect(within(welcome).getByText("Hats")).toBeInTheDocument();
+  expect(within(welcome).getByText("エンター")).toBeInTheDocument();
 });
