d719f387cd166c335666c5e75602ccfe66301621
Roie Natan <roie.natan@gmail.com>
2021-12-24 09:19:42 -0500

    christmas logo ; remove unneccessary mobile notification file

--- src/components/Navigation/Sidebar.tsx
+++ src/components/Navigation/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/icons/logo.icon";
+//import Logo from "../../assets/icons/logo.icon";
+import LogoChristmas from "../../assets/icons/logo-christmas.svg";
 import "../../styles/Sidebar.scss";
 import NavLinks from "./NavLinks";
 import SoicalAndLegal from "./SoicalAndLegal";
@@ -7,7 +8,7 @@ import SoicalAndLegal from "./SoicalAndLegal";
 export default function Sidebar() {
   return (
     <div className="sidebar-wrapper">
-      <Link to="/" className="logo"><Logo width="50px" /></Link>
+      <Link to="/" className="logo"><img src={LogoChristmas} alt="christams logo" /></Link>
       <NavLinks />
       <div className="bottom-wrapper">
         <SoicalAndLegal />
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-import Logo from "../assets/icons/logo.icon";
+//import Logo from "../assets/icons/logo.icon";
+import LogoChristmas from "../assets/icons/logo-christmas.svg";
 import { LocalStorage } from "../constants/constants";
 import "../styles/Welcome.scss";
 
@@ -27,7 +28,7 @@ export default function Welcome(props: IProps) {
   return (
     <div className="welcome-wrapper" data-testid="Welcome">
       <div className="welcome-content">
-        <Logo />
+        <img src={LogoChristmas} alt="christams logo" />
         <div className="title">{t("Welcome.title")}</div>
         <div className="description">{t("Welcome.description")}</div>
         <button className="enter-btn" onClick={seenWelcomePage}>
