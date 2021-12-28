8fb8f456386f633321242dd148715d68564c6803
Yehonatan Levi <leviyehonatan@gmail.com>
2021-12-28 14:06:30 +0200

    https://create-react-app.dev/docs/adding-custom-environment-variables/#what-other-env-files-can-be-used

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,4 @@
 import { DEFAULT_RINKEBY_SUBGRAPH_URI, LPUniswapURIs, Networks, MasterAddresses } from "./constants/constants";
-require('dotenv').config();
 
 export const NETWORK: Networks = process.env.REACT_APP_NETWORK as Networks || Networks.rinkeby;
 export const SUBGRAPH_URI = process.env.REACT_APP_SUBGRAPH_URI || DEFAULT_RINKEBY_SUBGRAPH_URI;
