b8c2561b20e2b9f2fada3dcbd31e033219c3a53d
bigwindev <bigwindev@gmail.com>
2022-01-13 10:13:26 +0200

    Implemented new styles for side menu of Comittee Tools

---
+++ src/assets/icons/plus.icon.js
@@ -0,0 +1,8 @@
+export default function PluseIcon(props) {
+  return (
+    <svg width={props.width ?? "14"} height={props.height ?? "14"} viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="6.36328" width="1.27273" height="14" fill="#8AFCFD"/>
+      <rect x="14" y="6.36353" width="1.27273" height="14" transform="rotate(90 14 6.36353)" fill="#8AFCFD"/>
+    </svg>
+  )
+}
--- src/components/CommitteeTools/KeysNavBar.tsx
+++ src/components/CommitteeTools/KeysNavBar.tsx
@@ -2,7 +2,9 @@ import classNames from "classnames";
 import { NavLink } from "react-router-dom";
 import { RoutePaths } from "../../constants/constants";
 import { IStoredKey } from "../../types/types";
+import ArrowIcon from '../../assets//icons/arrow.icon'
 import LockIcon from '../../assets//icons/lock.icon'
+import PlusIcon from '../../assets//icons/plus.icon'
 
 interface IProps {
     keys: IStoredKey[]
@@ -11,20 +13,29 @@ interface IProps {
 
 export default function KeysSidebar({ keys }: IProps) {
     return <div className={classNames({ 'keys-navbar': true })}>
+        <div className="keys-navbar-header">
+            <ArrowIcon />
+            <p>Stored keys</p>
+        </div>
         {
             keys && keys.map((storedKey) =>
                 <NavLink
                     key={storedKey.alias}
                     className="nav-link"
                     to={`${RoutePaths.committee_tools}/${storedKey.alias}`}
-                    activeClassName="selected"
-                ><LockIcon />{storedKey.alias}</NavLink>
+                    activeClassName="selected">
+                    <LockIcon width="16px" height="21px" />
+                    <p>{storedKey.alias}</p>
+                </NavLink>
             )
         }
         <NavLink
             exact
             className="nav-link"
-            activeClassName="selected"
-            to={`${RoutePaths.committee_tools}`}>new</NavLink>
+            to={`${RoutePaths.committee_tools}`}
+            activeClassName="selected">
+                <PlusIcon />
+                <p>New</p>
+            </NavLink>
     </div >
 }
