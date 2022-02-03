da05aa95aa8026b3274f20692dd0593a9b4c4fe5
Roie Natan <roie.natan@gmail.com>
2022-02-03 17:15:33 -0500

    show nft airdrop screen only if the user is connected to a wallet and the right network

--- src/components/NFTAirdrop/NFTAirdrop.tsx
+++ src/components/NFTAirdrop/NFTAirdrop.tsx
@@ -2,19 +2,19 @@ import { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Colors } from "../../constants/constants";
 import { RootState } from "../../reducers";
-import "./NFTAirdrop.scss";
 import { isAddress } from "ethers/lib/utils";
 import classNames from "classnames";
 import CloseIcon from "../../assets/icons/close.icon";
 import Redeem from "./Redeem";
 import { EligibleTokens } from "../../types/types";
-import { hashToken, linkToTokenEtherscan, normalizeAddress } from "../../utils";
+import { hashToken, isProviderAndNetwork, linkToTokenEtherscan, normalizeAddress } from "../../utils";
 import { useLocation } from "react-router-dom";
 import Loading from "../Shared/Loading";
 import { isRedeemed } from "../../actions/contractsActions";
 import OpenInIcon from "../../assets/icons/openIn.icon";
-import { NFT_AIRDROP_ADDRESS } from "../../settings";
+import { NETWORK, NFT_AIRDROP_ADDRESS } from "../../settings";
 import { useTranslation } from "react-i18next";
+import "./NFTAirdrop.scss";
 
 const { MerkleTree } = require('merkletreejs');
 const keccak256 = require('keccak256');
@@ -35,6 +35,7 @@ export default function NFTAirdrop() {
   const [merkleTree, setMerkleTree] = useState<any>();
   const [currentTokenID, setCurrentTokenID] = useState("");
   const [reveal, setReveal] = useState(false);
+  const provider = useSelector((state: RootState) => state.web3Reducer.provider);
   const { t } = useTranslation();
 
   const handleChange = useCallback(async (input: string) => {
@@ -90,36 +91,40 @@ export default function NFTAirdrop() {
 
   return (
     <div className={nftAirdropWrapperClass}>
-      <div className="nft-airdrop-search">
-        <h2>{eligibilityStatus === EligibilityStatus.ELIGIBLE ? "Congrats!" : eligibilityStatus === EligibilityStatus.NOT_ELIGIBLE ? "Ho no!" : "Hello"}</h2>
-        <span>{`Please connect to wallet or enter wallet address to check your eligibility for the NFT airdrop "The crow clan"`}</span>
-        <div className={inputContainerClass}>
-          <input className="address-input" type="text" value={userInput} placeholder="Enter wallet address" onChange={(e) => handleChange(e.target.value)} />
-          <button className="clear-input" onClick={() => handleChange("")}><CloseIcon width="10" height="10" fill={Colors.gray} /></button>
-        </div>
+      {isProviderAndNetwork(provider) ? (
+        <div className="nft-airdrop-search">
+          <h2>{eligibilityStatus === EligibilityStatus.ELIGIBLE ? "Congrats!" : eligibilityStatus === EligibilityStatus.NOT_ELIGIBLE ? "Ho no!" : "Hello"}</h2>
+          <span>{`Please connect to wallet or enter wallet address to check your eligibility for the NFT airdrop "The crow clan"`}</span>
+          <div className={inputContainerClass}>
+            <input className="address-input" type="text" value={userInput} placeholder="Enter wallet address" onChange={(e) => handleChange(e.target.value)} />
+            <button className="clear-input" onClick={() => handleChange("")}><CloseIcon width="10" height="10" fill={Colors.gray} /></button>
+          </div>
 
-        {userInput !== "" && !isAddress(userInput) && <span className="error-label">Please enter a valid address</span>}
-        {eligibilityStatus === EligibilityStatus.NOT_ELIGIBLE && <div className="info-label">This address is not part of the airdrop. Please check  the eligibility requirements here.</div>}
-        {eligibilityStatus === EligibilityStatus.ELIGIBLE && <div className="info-label">{t("NFTAirdop.eligible-text")}</div>}
-        {eligibilityStatus === EligibilityStatus.REDEEMED && (
-          <span className="open-in-etherscan-wrapper">
-            <span>Redeemed - View on Etherscan</span>
-            <span className="open-in-icon" onClick={() => window.open(linkToTokenEtherscan(NFT_AIRDROP_ADDRESS, currentTokenID))}>
-              <OpenInIcon />
+          {userInput !== "" && !isAddress(userInput) && <span className="error-label">Please enter a valid address</span>}
+          {eligibilityStatus === EligibilityStatus.NOT_ELIGIBLE && <div className="info-label">This address is not part of the airdrop. Please check  the eligibility requirements here.</div>}
+          {eligibilityStatus === EligibilityStatus.ELIGIBLE && <div className="info-label">{t("NFTAirdop.eligible-text")}</div>}
+          {eligibilityStatus === EligibilityStatus.REDEEMED && (
+            <span className="open-in-etherscan-wrapper">
+              <span>Redeemed - View on Etherscan</span>
+              <span className="open-in-icon" onClick={() => window.open(linkToTokenEtherscan(NFT_AIRDROP_ADDRESS, currentTokenID))}>
+                <OpenInIcon />
+              </span>
             </span>
-          </span>
-        )}
-      </div>
-      {(eligibilityStatus === EligibilityStatus.ELIGIBLE || eligibilityStatus === EligibilityStatus.REDEEMED) && (
-        <Redeem
-          merkleTree={merkleTree}
-          walletAddress={normalizeAddress(userInput)}
-          setPendingWalletAction={setPendingWalletAction}
-          onSuccess={() => setEligibilityStatus(EligibilityStatus.REDEEMED)}
-          eligibilityStatus={eligibilityStatus}
-          reveal={reveal} />
-      )}
+          )}
+        </div>
+      ) : <span>{`Please connect your wallet to ${NETWORK}`}</span>}
+      {
+        (eligibilityStatus === EligibilityStatus.ELIGIBLE || eligibilityStatus === EligibilityStatus.REDEEMED) && (
+          <Redeem
+            merkleTree={merkleTree}
+            walletAddress={normalizeAddress(userInput)}
+            setPendingWalletAction={setPendingWalletAction}
+            onSuccess={() => setEligibilityStatus(EligibilityStatus.REDEEMED)}
+            eligibilityStatus={eligibilityStatus}
+            reveal={reveal} />
+        )
+      }
       {pendingWalletAction && <Loading />}
-    </div>
+    </div >
   )
 }
