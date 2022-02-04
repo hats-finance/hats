747214815f5e60b33936dd034a017caefbfd7dda
mwogates <michaelwonggates@gmail.com>
2022-02-04 10:59:53 -0500

    feat: update editable content component and its style, config src baseurl

--- src/components/CommitteeTools/components/Decrypt/Decrypt.tsx
+++ src/components/CommitteeTools/components/Decrypt/Decrypt.tsx
@@ -7,136 +7,170 @@ import { decryptKey, readPrivateKey } from "openpgp";
 import SelectKeyModal from "../SelectKeyModal/SelectKeyModal";
 import { useLocation } from "react-router-dom";
 import EditableContent from "../EditableContent/EditableContent";
-import CopyIcon from '../../../../assets/icons/copy.icon.svg'
+import CopyIcon from "assets/icons/copy.icon.svg";
 
-export async function readPrivateKeyFromStoredKey(privateKey: string, passphrase: string | undefined) {
-  return passphrase ? await decryptKey({
-    privateKey: await readPrivateKey({ armoredKey: privateKey }),
-    passphrase
-  }) : await readPrivateKey({ armoredKey: privateKey })
+export async function readPrivateKeyFromStoredKey(
+  privateKey: string,
+  passphrase: string | undefined
+) {
+  return passphrase
+    ? await decryptKey({
+        privateKey: await readPrivateKey({ armoredKey: privateKey }),
+        passphrase
+      })
+    : await readPrivateKey({ armoredKey: privateKey });
 }
 
-
 export default function Decrypt() {
-  const vaultContext = useContext(VaultContext)
-  const [showSelectKeyModal, setShowSelectKeyModal] = useState(false)
-  const [showSelectedKeyDetails, setShowSelectedKeyDetails] = useState(false)
+  const vaultContext = useContext(VaultContext);
+  const [showSelectKeyModal, setShowSelectKeyModal] = useState(false);
+  const [showSelectedKeyDetails, setShowSelectedKeyDetails] = useState(false);
   const [error, setError] = useState<string>();
-  const encryptedMessageRef = useRef<HTMLDivElement>(null);
-  const decryptedMessageRef = useRef<HTMLDivElement>(null);
+  const encryptedMessageRef = useRef<HTMLTextAreaElement>(null);
+  const decryptedMessageRef = useRef<HTMLTextAreaElement>(null);
   const { t } = useTranslation();
 
-
-  const location = useLocation()
+  const location = useLocation();
 
   useEffect(() => {
     const urlSearchParams = new URLSearchParams(location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
     if (params.ipfsjson) {
       (async () => {
-        const response = await fetch(params.ipfsjson)
-        const json = await response.json()
-        encryptedMessageRef.current!.textContent = json.message
-
-      })()
-
+        const response = await fetch(params.ipfsjson);
+        const json = await response.json();
+        encryptedMessageRef.current!.value = json.message;
+      })();
     }
-
-  }, [location.search])
+  }, [location.search]);
 
   useEffect(() => {
-    if (vaultContext.vault?.storedKeys.length === 0 || !vaultContext.selectedKey === undefined) {
-      setShowSelectKeyModal(true)
+    if (
+      vaultContext.vault?.storedKeys.length === 0 ||
+      !vaultContext.selectedKey === undefined
+    ) {
+      setShowSelectKeyModal(true);
     }
-  }, [vaultContext.vault, vaultContext.selectedKey])
+  }, [vaultContext.vault, vaultContext.selectedKey]);
 
   const _decrypt = useCallback(async () => {
     try {
       if (!vaultContext.selectedKey) {
-        setShowSelectKeyModal(true)
-        return
+        setShowSelectKeyModal(true);
+        return;
       }
 
-      const armoredMessage = encryptedMessageRef.current!.textContent
+      const armoredMessage = encryptedMessageRef.current!.value;
 
       if (!armoredMessage || armoredMessage === "") {
-        throw new Error("No message to decrypt")
+        throw new Error("No message to decrypt");
       }
 
       const privateKey = await readPrivateKeyFromStoredKey(
         vaultContext.selectedKey.privateKey,
         vaultContext.selectedKey.passphrase
-      )
-      const message = await readMessage({ armoredMessage })
-      const { data: decrypted } = await decrypt({ message, decryptionKeys: privateKey })
-      decryptedMessageRef.current!.textContent = decrypted
+      );
+      const message = await readMessage({ armoredMessage });
+      const { data: decrypted } = await decrypt({
+        message,
+        decryptionKeys: privateKey
+      });
+      decryptedMessageRef.current!.value = decrypted;
     } catch (error) {
       if (error instanceof Error) {
-        setError(error.message)
+        setError(error.message);
       } else {
-        console.log(error)
+        console.log(error);
       }
     }
-  }, [vaultContext.selectedKey])
+  }, [vaultContext.selectedKey]);
 
   const _encrypt = useCallback(async () => {
     try {
-      setError("")
+      setError("");
       if (!vaultContext.selectedKey) {
-        setShowSelectKeyModal(true)
-        return
+        setShowSelectKeyModal(true);
+        return;
       }
       const privateKey = await readPrivateKeyFromStoredKey(
         vaultContext.selectedKey.privateKey,
         vaultContext.selectedKey.passphrase
-      )
-      const message = await createMessage({ text: decryptedMessageRef.current!.textContent! })
-      encryptedMessageRef.current!.textContent = await encrypt({ message, encryptionKeys: privateKey?.toPublic() })
+      );
+      const message = await createMessage({
+        text: decryptedMessageRef.current!.value!
+      });
+      encryptedMessageRef.current!.value = await encrypt({
+        message,
+        encryptionKeys: privateKey?.toPublic()
+      });
     } catch (error) {
       if (error instanceof Error) {
-        setError(error.message)
+        setError(error.message);
       }
     }
-  }, [vaultContext.selectedKey])
+  }, [vaultContext.selectedKey]);
 
-  const SelectedKeypair = () => (
-    vaultContext &&
-    <div className="selected-key">
-      <div className="box-with-copy">
-        <span>{vaultContext.selectedKey ?
-          vaultContext.selectedKey.alias :
-          t("CommitteeTools.Decrypt.no-key-selected")}
-        </span>
-        {vaultContext.selectedKey &&
-          <img alt="copy" src={CopyIcon} onClick={() => {
-            setShowSelectedKeyDetails(true)
-          }} />}
+  const SelectedKeypair = () =>
+    vaultContext && (
+      <div className="selected-key">
+        <div className="box-with-copy">
+          <span>
+            {vaultContext.selectedKey
+              ? vaultContext.selectedKey.alias
+              : t("CommitteeTools.Decrypt.no-key-selected")}
+          </span>
+          {vaultContext.selectedKey && (
+            <img
+              alt="copy"
+              src={CopyIcon}
+              onClick={() => {
+                setShowSelectedKeyDetails(true);
+              }}
+            />
+          )}
+        </div>
+        <button
+          onClick={() => {
+            setShowSelectKeyModal(true);
+          }}
+        >
+          {t("CommitteeTools.Decrypt.select-keypair")}
+        </button>
       </div>
-      <button onClick={() => {
-        setShowSelectKeyModal(true)
-      }}>{t("CommitteeTools.Decrypt.select-keypair")}</button>
-    </div>)
-
+    );
 
   return (
-    <div>
+    <div className="decrypt-wrapper">
       <SelectedKeypair />
-      <p>{t("CommitteeTools.Decrypt.encrypted-message")}</p>
-      <EditableContent pastable ref={encryptedMessageRef} />
-      {error && <p>{error}</p>}
-      <div><button onClick={_decrypt}>Decrypt</button></div>
-      <p>{t("CommitteeTools.Decrypt.decrypted-message")}</p>
-      <EditableContent copyable ref={decryptedMessageRef} />
-      <div><button onClick={_encrypt}>Encrypt</button></div>
-      {(showSelectKeyModal || showSelectedKeyDetails) && <SelectKeyModal
-        showKey={showSelectedKeyDetails ? vaultContext.selectedKey! : undefined}
-        setShowModal={() => {
-          if (showSelectedKeyDetails)
-            setShowSelectedKeyDetails(false)
-          if (showSelectKeyModal)
-            setShowSelectKeyModal(false)
-        }} />}
-    </div>
-  )
-}
 
+      <div className="decrypt-wrapper__textbox-container">
+        <p className="decrypt-wrapper__textbox-title">
+          {t("CommitteeTools.Decrypt.encrypted-message")}
+        </p>
+        <EditableContent pastable ref={encryptedMessageRef} />
+        {error && <p>{error}</p>}
+        <button onClick={_decrypt}>Decrypt</button>
+      </div>
+
+      <div className="decrypt-wrapper__textbox-container">
+        <p className="decrypt-wrapper__textbox-title">
+          {t("CommitteeTools.Decrypt.decrypted-message")}
+        </p>
+        <EditableContent copyable ref={decryptedMessageRef} />
+        <button onClick={_encrypt}>Encrypt</button>
+      </div>
+
+      {(showSelectKeyModal || showSelectedKeyDetails) && (
+        <SelectKeyModal
+          showKey={
+            showSelectedKeyDetails ? vaultContext.selectedKey! : undefined
+          }
+          setShowModal={() => {
+            if (showSelectedKeyDetails) setShowSelectedKeyDetails(false);
+            if (showSelectKeyModal) setShowSelectKeyModal(false);
+          }}
+        />
+      )}
+    </div>
+  );
+}
--- src/components/CommitteeTools/components/EditableContent/EditableContent.tsx
+++ src/components/CommitteeTools/components/EditableContent/EditableContent.tsx
@@ -1,42 +1,51 @@
-import './index.scss'
-import PasteIcon from '../../../../assets/icons/paste.icon.svg'
-import CopyIcon from '../../../../assets/icons/copy.icon.svg'
-import { forwardRef } from 'react'
-function EditableContent({ pastable, copyable, ...props }:
-    {
-        pastable?: boolean
-        copyable?: boolean
-        placeholder?: string
-    }, ref) {
-    const extraIcons = pastable || copyable
-    return (
-        <div className="pastable-content">
-            <div className='content-wrapper'>
-                <div
-                    ref={ref}
-                    {...props}
-                    contentEditable >
-                </div>
-            </div>
-            {extraIcons &&
-                <div className="extra-icons">
-                    {pastable && <img
-                        alt="paste"
-                        src={PasteIcon}
-                        onClick={() => {
-                            navigator.clipboard.readText().then(text => {
-                                ref.current!.textContent = text
-                            })
-                        }} />}
-                    {copyable && <img
-                        alt="copy"
-                        src={CopyIcon}
-                        onClick={() => {
-                            navigator.clipboard.writeText(ref.current!.textContent)
-                        }} />}
-                </div>}
+import { forwardRef } from "react";
+import "./index.scss";
+import PasteIcon from "assets/icons/paste.icon.svg";
+import CopyIcon from "assets/icons/copy.icon.svg";
+
+function EditableContent(
+  {
+    pastable,
+    copyable,
+    ...props
+  }: {
+    pastable?: boolean;
+    copyable?: boolean;
+    placeholder?: string;
+  },
+  ref
+) {
+  const extraIcons = pastable || copyable;
+
+  return (
+    <div className="pastable-content">
+      <textarea ref={ref} {...props} className="pastable-content__textarea" />
+      {extraIcons && (
+        <div className="pastable-content__extra-icons">
+          {pastable && (
+            <img
+              alt="paste"
+              src={PasteIcon}
+              onClick={() => {
+                navigator.clipboard.readText().then((text) => {
+                  ref.current!.value = text;
+                });
+              }}
+            />
+          )}
+          {copyable && (
+            <img
+              alt="copy"
+              src={CopyIcon}
+              onClick={() => {
+                navigator.clipboard.writeText(ref.current!.value);
+              }}
+            />
+          )}
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default forwardRef(EditableContent)
+export default forwardRef(EditableContent);
