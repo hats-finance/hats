b0df572df96174c9630c2e4c9974f53bf7015a2e
Roie Natan <roie.natan@gmail.com>
2022-02-10 09:47:57 -0500

    remove nft airdrop address env var

--- src/settings.ts
+++ src/settings.ts
@@ -5,7 +5,7 @@ export const NETWORK: Networks = process.env.REACT_APP_NETWORK as Networks || Ne
 export const SUBGRAPH_URI = process.env.REACT_APP_SUBGRAPH_URI || DEFAULT_RINKEBY_SUBGRAPH_URI;
 export const LP_UNISWAP_URI = process.env.REACT_APP_LP_UNISWAP_URI || LPUniswapURIs[NETWORK];
 export const MASTER_ADDRESS = process.env.REACT_APP_MASTER_ADDRESS || MasterAddresses[NETWORK];
-export const NFT_AIRDROP_ADDRESS = process.env.REACT_NFT_AIRDROP_ADDRESS || NFTAirdropAddress[NETWORK];
+export const NFT_AIRDROP_ADDRESS = NFTAirdropAddress[NETWORK] || NFTAirdropAddress.main;
 export const DATA_POLLING_INTERVAL = 10000;
 
 /** Currently not in use */
