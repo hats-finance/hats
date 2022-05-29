c647f628bafd9e950582ae8a7674f21b9b52deb7
Yehonatan Levi <leviyehonatan@gmail.com>
2022-05-29 14:33:35 +0300

    clean

--- src/components/BountyPayout/VaultSelector.tsx
+++ src/components/BountyPayout/VaultSelector.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../reducers";
 import SearchIcon from "../../assets/icons/search.icon";
