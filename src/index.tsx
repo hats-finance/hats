538a24ac8bb6c2ad7153f51edd768dadf03eca62
Yehonatan Levi <leviyehonatan@gmail.com>
2022-06-15 10:03:17 +0300

    remove comment

--- src/index.tsx
+++ src/index.tsx
@@ -28,11 +28,3 @@ root.render(
   </DAppProvider>,
 
 );
-
-/** Currently not in use - this is to support multiple subgraphs */
-
-// const lp_uniswap_subgraph = new HttpLink({
-//   uri: LP_UNISWAP_URI
-// });
-
-//const apolloLink = ApolloLink.split(operation => operation.getContext().clientName === LP_UNISWAP_V3_HAT_ETH_APOLLO_CONTEXT, lp_uniswap_subgraph, main_subgraph);
