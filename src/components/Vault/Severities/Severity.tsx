ab3a0a7127759d9f5a3f9b10c579616a642582e1
Yehonatan Levi <leviyehonatan@gmail.com>
2022-06-28 18:34:09 +0300

    fix severity reward

--- src/components/Vault/Severities/Severity.tsx
+++ src/components/Vault/Severities/Severity.tsx
@@ -28,11 +28,11 @@ export default function Severity(props: IProps) {
   const [modalContractsData, setModalContractsData] = useState(null);
   const { rewardsLevels, honeyPotBalance, stakingTokenDecimals, hackerVestedRewardSplit, hackerRewardSplit,
     committeeRewardSplit, swapAndBurnSplit, governanceHatRewardSplit, hackerHatRewardSplit, vestingDuration,
-    stakingTokenSymbol } = props.vault;
+    stakingToken, stakingTokenSymbol } = props.vault;
   const { severityIndex, severity, expanded, expandedSeverityIndex } = props;
   const screenSize = useSelector((state: RootState) => state.layoutReducer.screenSize);
   const rewardPercentage = (Number(rewardsLevels[severity.index]) / 10000) * 100;
-  const tokenPrice = useSelector((state: RootState) => state.dataReducer.tokenPrices)?.[stakingTokenSymbol];
+  const tokenPrice = useSelector((state: RootState) => state.dataReducer.tokenPrices)?.[stakingToken];
   const rewardPrice = calculateRewardPrice(rewardPercentage, tokenPrice, honeyPotBalance, stakingTokenDecimals);
 
   return (
