58f9b9d7f34a4888fe5953e07b745d4a22d818d0
Roie Natan <roie.natan@gmail.com>
2022-06-30 16:31:15 +0400

    don't show unregistered vaults

--- src/components/Honeypots.tsx
+++ src/components/Honeypots.tsx
@@ -50,9 +50,11 @@ export default function Honeypots({ showDeposit }: IProps) {
 
   const normalVaultKey: string = ''
 
-  let vaultsByGroup = vaultsMatchSearch?.reduce((groups, vault) => {
+  const vaultsByGroup = vaultsMatchSearch?.reduce((groups, vault) => {
     const key = vault.description?.["project-metadata"].type || normalVaultKey;
-    (groups[key] = groups[key] || []).push(vault);
+    if (vault.registered) {
+      (groups[key] = groups[key] || []).push(vault);
+    }
     return groups;
   }, [] as IVault[][])!
 
