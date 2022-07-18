f7b592ee800c9e37914c5604b1b8afeb7322dfe2
Roie Natan <roie.natan@gmail.com>
2022-07-18 11:39:22 +0400

    updates

--- src/components/MyAccount/MyAccount.tsx
+++ src/components/MyAccount/MyAccount.tsx
@@ -1,9 +1,17 @@
+import { shortenIfAddress, useEthers } from "@usedapp/core";
+import { useTranslation } from "react-i18next";
 import "./index.scss";
 
 export default function MyAccount() {
+  const { t } = useTranslation();
+  const { account } = useEthers()
+
   return (
     <div className="my-account-wrapper">
-      MyAccount
+      <div className="my-account__wallet">
+        <span className="my-account__hello">{t("MyAccount.hello")},</span>
+        <span className="my-account__address">{shortenIfAddress(account)}</span>
+      </div>
     </div>
   )
 }
