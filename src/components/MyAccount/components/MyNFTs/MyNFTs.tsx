c34ff5be214b6b0be3de9adb945b0c5c63a592be
Roie Natan <roie.natan@gmail.com>
2022-07-26 16:50:10 +0400

    add redeem all buttons options in my account component

--- src/components/MyAccount/components/MyNFTs/MyNFTs.tsx
+++ src/components/MyAccount/components/MyNFTs/MyNFTs.tsx
@@ -1,39 +1,47 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { useVaults } from "hooks/useVaults";
+import { useTranslation } from "react-i18next";
+import classNames from "classnames";
+import Loading from "components/Shared/Loading";
 import "./index.scss";
 import "swiper/css";
 
 export default function MyNFTs() {
+  const { t } = useTranslation();
   const { nftData } = useVaults();
 
   const nfts = nftData?.redeemable?.map((nft, index) =>
     <SwiperSlide key={index}>
       <img key={index} src={nft.tokenUri} alt="nft" />
-      {/* TODO: need to assign to correct redeem function (Tree or Shares)
-                But maybe we need just two buttons, one to redeem all from shares
-                and one to reedem all from tree.
-      */}
-      {!nft.isRedeemed && (
-        <button className="fill">
-          Redeem
-        </button>
-      )}
     </SwiperSlide>
   )
 
+  const airdropToRedeem = nftData?.redeemable?.filter(nft => nft.type === "MerkleTree")?.some(nft => !nft.isRedeemed);
+  const depositToRedeem = nftData?.redeemable?.filter(nft => nft.type === "Deposit")?.some(nft => !nft.isRedeemed);
+  const showLoader =
+    nftData?.redeemMultipleFromTreeState.status === "PendingSignature" ||
+    nftData?.redeemMultipleFromTreeState.status === "Mining" ||
+    nftData?.redeemMultipleFromSharesState.status === "PendingSignature" ||
+    nftData?.redeemMultipleFromSharesState.status === "Mining";
+
   return (
-    <div className="my-nfts-wrapper">
-      {nfts?.length === 0 ? "No NFTs yet" : (
-        <Swiper
-          spaceBetween={1}
-          slidesPerView={3}
-          speed={500}
-          touchRatio={1.5}
-          navigation={true}
-          effect={"flip"}>
-          {nfts}
-        </Swiper>
-      )}
+    <div className={classNames("my-nfts-wrapper", { "disabled": showLoader })}>
+      <div className="my-nfts__container">
+        {nfts?.length === 0 ? "No NFTs yet" : (
+          <Swiper
+            spaceBetween={1}
+            slidesPerView={3}
+            speed={500}
+            touchRatio={1.5}
+            navigation={true}
+            effect={"flip"}>
+            {nfts}
+          </Swiper>
+        )}
+      </div>
+      {airdropToRedeem && <button onClick={nftData?.redeemTree} className="my-nfts__action-btn fill">{t("MyAccount.MyNFTs.airdrop-redeem")}</button>}
+      {depositToRedeem && <button onClick={nftData?.redeemShares} className="my-nfts__action-btn fill">{t("MyAccount.MyNFTs.deposit-redeem")}</button>}
+      {showLoader && <Loading />}
     </div>
   )
 }
