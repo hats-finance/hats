777bfee111fd46ef5824038b22bd232a389ec631
Roie Natan <roie.natan@gmail.com>
2022-09-05 09:36:34 +0800

    handle special pids taht start with 8 and 9

--- src/components/RedeemNftSuccess/RedeemNftSuccess.tsx
+++ src/components/RedeemNftSuccess/RedeemNftSuccess.tsx
@@ -22,10 +22,16 @@ export default function RedeemNftSuccess({ type }: IProps) {
   const { t } = useTranslation();
   const { nftData, vaults } = useVaults();
 
-  const vaultsNames: string[] = [];
+  const vaultsNames = new Set();
   const nfts = nftData?.nftTokens?.filter(nft => nft[type] && nft.isRedeemed).map((nft, index) => {
-    const nftVaultName = (vaults?.find(vault => vault.pid === String(nft.pid)))?.description?.["project-metadata"].name;
-    if (nftVaultName) vaultsNames.push(nftVaultName);
+
+    /** this regex is to handle the special pids that begin with 8 and 9 */
+    const formattedPid = /[8-9]?0*([0-9]+)$/gm.exec(String(nft.pid));
+    if (formattedPid) {
+      const nftVaultName = (vaults?.find(vault => vault.pid === formattedPid[1]))?.description?.["project-metadata"].name;
+      if (nftVaultName) vaultsNames.add(nftVaultName);
+    }
+
     return (
       <SwiperSlide key={index} className="swiper-slide">
         <NFTMedia key={index} link={ipfsTransformUri(nft.nftInfo.image)} />
@@ -38,7 +44,7 @@ export default function RedeemNftSuccess({ type }: IProps) {
       <img className="airdrop-redeem-success__icon" src={RedeemWalletSuccessIcon} alt="wallet" />
       <div className="airdrop-redeem-success__title">{t("RedeemNftSuccess.title")}</div>
       <span className="airdrop-redeem-success__sub-title">{t("RedeemNftSuccess.sub-title")}</span>
-      {`${t("RedeemNftSuccess.text-1")} ${vaultsNames.join(', ').replace(/,(?!.*,)/gmi, `${t("RedeemNftSuccess.text-8")}`)} ${t("RedeemNftSuccess.text-2")}`}
+      {`${t("RedeemNftSuccess.text-1")} ${Array.from(vaultsNames).join(', ').replace(/,(?!.*,)/gmi, ` ${t("RedeemNftSuccess.text-8")}`)} ${t("RedeemNftSuccess.text-2")}`}
       {t("RedeemNftSuccess.text-3")}
       <b>{t("RedeemNftSuccess.text-4")}</b>
       <div className="airdrop-redeem-success__features-wrapper">
