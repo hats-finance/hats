fb0b68edcf3096e4b22ec775b65e495345b74a5a
Yehonatan Levi <leviyehonatan@gmail.com>
2022-09-06 12:38:08 +0200

    added open sea link in full screen

--- src/components/NFTCard/NFTCard.tsx
+++ src/components/NFTCard/NFTCard.tsx
@@ -5,6 +5,8 @@ import Media from "components/Shared/Media/Media";
 import { INFTTokenInfo } from "types/types";
 import classNames from "classnames";
 import { useTranslation } from "react-i18next";
+import { useEthers } from "@usedapp/core";
+import { HATVaultsNFTContract } from "constants/constants";
 
 interface IProps {
   tokenInfo: INFTTokenInfo
@@ -12,17 +14,28 @@ interface IProps {
 }
 
 export default function NFTCard({ tokenInfo, width }: IProps) {
-  const { metadata, isRedeemed } = tokenInfo;
+  const { metadata, isRedeemed, tokenId } = tokenInfo;
+  const { chainId } = useEthers();
   const { t } = useTranslation();
   const [fullScreen, setFullScreen] = useState(false);
   const tier = metadata.attributes.find(attr => attr.trait_type === "Trust Level")?.value;
   const vaultName = metadata.attributes.find(attr => attr.trait_type === "Vault")?.value;
+
+  let openSeaUrl;
+  const nftContract = HATVaultsNFTContract[chainId!];
+  if (chainId === 1) {
+    openSeaUrl = `https://opensea.io/assets/${nftContract}/${tokenId}`;
+  }
+  else if (chainId === 4) {
+    openSeaUrl = `https://testnets.opensea.io/assets/${nftContract}/${tokenId}`;
+  }
 
   if (fullScreen) {
     return (
       ReactDOM.createPortal(
         <div className="nft-card-full-screen-wrapper">
           <button onClick={() => setFullScreen(false)} className="nft-card-full-screen__close-btn">&times;</button>
+          {isRedeemed && <a href={openSeaUrl} target="_blank" rel="noreferrer">View on OpenSea</a>}
           <div className="nft-card-full-screen__container">
             <Media link={metadata.animation_url} ipfsLink width="100%" />
           </div>
