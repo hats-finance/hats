06a20e8e8729383fcc3115d4f9a98a589025bbf7
Carlos Fontes <fonstack@gmail.com>
2022-10-07 15:02:24 +0100

    created media query definitions to speed up

--- src/styles/breakpoints.styles.ts
+++ src/styles/breakpoints.styles.ts
@@ -7,3 +7,8 @@ export const breakpointsDefinition: BreakpointsDefinition = {
   smallScreen: '100rem', // 1600px
   mobile: '68.75rem', // 1100px
 };
+
+export const mediaQuery: BreakpointsDefinition = {
+  smallScreen: `only screen and (max-width: ${breakpointsDefinition.smallScreen})`,
+  mobile: `only screen and (max-width: ${breakpointsDefinition.mobile})`,
+};
