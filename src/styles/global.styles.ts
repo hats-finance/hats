a585d14b91959db3f0498032d27057dac03df38b
Carlos Fontes <fonstack@gmail.com>
2022-10-11 16:24:39 +0100

    solved some issues with UI

--- src/styles/global.styles.ts
+++ src/styles/global.styles.ts
@@ -40,6 +40,7 @@ export const GlobalStyle = createGlobalStyle`
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         letter-spacing: 1px;
+        overflow: hidden;
     }
 
     .vaults-wrapper {
