2817faa0d833a79142e087ebb6d52c3e522daaeb
Carlos Fontes <fonstack@gmail.com>
2022-10-11 18:01:00 +0100

    refactor CSS KeyManager

--- src/components/Keystore/KeyManager/KeyManager.tsx
+++ src/components/Keystore/KeyManager/KeyManager.tsx
@@ -1,7 +1,8 @@
-import { KeystoreContext, SelectKeyModal, UnlockKeystoreModal, CreateKeystoreModal } from "components/Keystore";
 import { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { KeystoreContext, SelectKeyModal, UnlockKeystoreModal, CreateKeystoreModal } from "components/Keystore";
 import CopyIcon from "assets/icons/copy.icon.svg";
+import { StyledKeyManager } from "./styles";
 
 export function KeyManager({ onSelected }) {
   const [showSelectKeyModal, setShowSelectKeyModal] = useState(false);
@@ -13,41 +14,27 @@ export function KeyManager({ onSelected }) {
   const { t } = useTranslation();
 
   useEffect(() => {
-    onSelected(keystoreContext.selectedKey)
+    onSelected(keystoreContext.selectedKey);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [keystoreContext.selectedKey])
+  }, [keystoreContext.selectedKey]);
 
   const SelectedKeypair = () =>
     keystoreContext && (
-      <div className="decrypt-wrapper__selected-key">
+      <StyledKeyManager>
         <div className="box-with-copy">
           <div className="selected-key">
             <div className="selected-key__fish-eye" />
             <span>
-              {keystoreContext.selectedKey
-                ? keystoreContext.selectedKey.alias
-                : t("CommitteeTools.Decrypt.no-key-selected")}
+              {keystoreContext.selectedKey ? keystoreContext.selectedKey.alias : t("CommitteeTools.Decrypt.no-key-selected")}
             </span>
           </div>
-          {keystoreContext.selectedKey && (
-            <img
-              alt="copy"
-              src={CopyIcon}
-              onClick={() => {
-                setShowSelectedKeyDetails(true);
-              }}
-            />
-          )}
+          {keystoreContext.selectedKey && <img alt="copy" src={CopyIcon} onClick={() => setShowSelectedKeyDetails(true)} />}
         </div>
-        <button
-          className="open-key-list fill"
-          onClick={() => {
-            setShowSelectKeyModal(true);
-          }}
-        >
+        
+        <button className="open-key-list fill" onClick={() => setShowSelectKeyModal(true)}>
           {t("CommitteeTools.Decrypt.select-keypair")}
         </button>
-      </div>
+      </StyledKeyManager>
     );
 
   return (
@@ -55,28 +42,30 @@ export function KeyManager({ onSelected }) {
       {!keystoreContext.isLocked && <SelectedKeypair />}
       {(showSelectKeyModal || showSelectedKeyDetails) && (
         <SelectKeyModal
-          showKey={
-            showSelectedKeyDetails ? keystoreContext.selectedKey : undefined
-          }
+          showKey={showSelectedKeyDetails ? keystoreContext.selectedKey : undefined}
           setShowModal={() => {
             if (showSelectedKeyDetails) setShowSelectedKeyDetails(false);
             if (showSelectKeyModal) setShowSelectKeyModal(false);
           }}
         />
       )}
-      {keystoreContext.isCreated && keystoreContext.isLocked && (<>
-        <button onClick={() => setShowUnlockVault(true)} className="fill">{t("CommitteeTools.unlock-vault")}</button>
-      </>)}
+      {keystoreContext.isCreated && keystoreContext.isLocked && (
+        <>
+          <button onClick={() => setShowUnlockVault(true)} className="fill">
+            {t("CommitteeTools.unlock-vault")}
+          </button>
+        </>
+      )}
 
-      {!keystoreContext.isCreated && (<>
-        <button onClick={() => setShowCreateVault(true)} className="fill">{t("CommitteeTools.create-vault")}</button>
-      </>)}
-      {showCreateVault && !keystoreContext.isCreated && (
-        <CreateKeystoreModal setShowModal={setShowCreateVault} />
+      {!keystoreContext.isCreated && (
+        <>
+          <button onClick={() => setShowCreateVault(true)} className="fill">
+            {t("CommitteeTools.create-vault")}
+          </button>
+        </>
       )}
-      {showUnlockVault && keystoreContext.isLocked && (
-        <UnlockKeystoreModal />
-      )}
+      {showCreateVault && !keystoreContext.isCreated && <CreateKeystoreModal setShowModal={setShowCreateVault} />}
+      {showUnlockVault && keystoreContext.isLocked && <UnlockKeystoreModal />}
     </div>
-  )
+  );
 }
