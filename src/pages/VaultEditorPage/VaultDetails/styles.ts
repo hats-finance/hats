699d02f01601a6e84110f21bbe1eda183b146e84
Carlos Fontes <fonstack@gmail.com>
2022-10-11 16:41:32 +0100

    UI fixes on IconEditor

--- src/pages/VaultEditorPage/VaultDetails/styles.ts
+++ src/pages/VaultEditorPage/VaultDetails/styles.ts
@@ -1,19 +1,37 @@
+import { breakpointsDefinition } from "./../../../styles/breakpoints.styles";
 import styled from "styled-components";
 
 export const StyledVaultDetails = styled.div`
   display: flex;
   flex-wrap: wrap;
 
-  .inputs, .icons {
+  .inputs,
+  .icons {
     width: 50%;
+
+    @media (max-width: ${breakpointsDefinition.mobile}) {
+      width: 100%;
+    }
   }
 
   .icons {
     display: flex;
     justify-content: flex-end;
+    
+    @media (max-width: ${breakpointsDefinition.mobile}) {
+      width: 100%;
+    }
 
     &__input {
-        margin-left: 30px;
+      margin-left: 30px;
+
+      @media (max-width: ${breakpointsDefinition.mobile}) {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        margin-left: 0;
+      }
     }
   }
 `;
