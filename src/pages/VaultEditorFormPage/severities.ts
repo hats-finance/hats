e0226a13a8a2218721ca935e44d3a212604178b4
Yehonatan Levi <leviyehonatan@gmail.com>
2022-10-12 19:54:10 +0300

    types for new forms

--- src/pages/VaultEditorFormPage/severities.ts
+++ src/pages/VaultEditorFormPage/severities.ts
@@ -1,58 +1,61 @@
-import { IVulnerabilitySeverity } from "types/types"
+import { ISeveritiesTemplate } from "./types"
+export const severities: ISeveritiesTemplate = {
+    name: "Default Template",
+    indexArray: [0, 10, 20, 70, 150, 250, 300, 400, 500, 600, 1000, 1200, 1400, 1800, 2000, 2500, 3000, 4000, 5000, 6000, 8000],
+    severities: [{
+        "name": "low",
+        "index": 7,
+        "contracts-covered": [],
+        "nft-metadata": {
+            "name": "Good citizen",
+            "description": " Vault low severity reward",
+            "animation_url": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSize-Low-Tech%20Sphere.mp4",
+            "image": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSize-Low-Tech%20Sphere.mp4",
+            "external_url": ""
 
-export const severities: IVulnerabilitySeverity[] = [{
-    "name": "low",
-    "index": 7,
-    "contracts-covered": [],
-    "nft-metadata": {
-        "name": "Good citizen",
-        "description": " Vault low severity reward",
-        "animation_url": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSize-Low-Tech%20Sphere.mp4",
-        "image": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSize-Low-Tech%20Sphere.mp4",
-        "external_url": ""
-
+        },
+        "description": "Contract does not function as expected, with no loss of funds. Prize will be capped to 5% of the amount that could be frozen, extracted or at risk in production at the time of disclosure."
     },
-    "description": "Contract does not function as expected, with no loss of funds. Prize will be capped to 5% of the amount that could be frozen, extracted or at risk in production at the time of disclosure."
-},
-{
-    "name": "medium",
-    "index": 14,
-    "contracts-covered": [],
-    "nft-metadata": {
-        "name": "Protocol protector",
-        "description": " Vault medium severity reward",
-        "animation_url": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSize-%20Medium-%20Electric%20Dreams.mp4",
-        "image": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSize-%20Medium-%20Electric%20Dreams.mp4",
-        "external_url": ""
+    {
+        "name": "medium",
+        "index": 14,
+        "contracts-covered": [],
+        "nft-metadata": {
+            "name": "Protocol protector",
+            "description": " Vault medium severity reward",
+            "animation_url": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSize-%20Medium-%20Electric%20Dreams.mp4",
+            "image": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSize-%20Medium-%20Electric%20Dreams.mp4",
+            "external_url": ""
 
+        },
+        "description": "Contract consumes unbounded gas,block stuffing, griefing denial of service (i.e. attacker spends as much in gas as damage to the contract), gas griefing. Prize will be capped to 20% of the amount that could be frozen, extracted or at risk in production at the time of disclosure."
     },
-    "description": "Contract consumes unbounded gas,block stuffing, griefing denial of service (i.e. attacker spends as much in gas as damage to the contract), gas griefing. Prize will be capped to 20% of the amount that could be frozen, extracted or at risk in production at the time of disclosure."
-},
-{
-    "name": "high",
-    "index": 16,
-    "contracts-covered": [],
-    "nft-metadata": {
-        "name": "Protocol champion",
-        "description": " high severity reward",
-        "animation_url": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSizeHigh-%20Altair.mp4",
-        "image": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSizeHigh-%20Altair.mp4",
+    {
+        "name": "high",
+        "index": 16,
+        "contracts-covered": [],
+        "nft-metadata": {
+            "name": "Protocol champion",
+            "description": " high severity reward",
+            "animation_url": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSizeHigh-%20Altair.mp4",
+            "image": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSizeHigh-%20Altair.mp4",
 
-        "external_url": ""
+            "external_url": ""
+        },
+        "description": "Token holders temporarily unable to transfer holdings ,users spoof each other, theft of yield - Transient consensus failures. Prize will be capped to 30% of the amount that could be frozen, extracted or at risk in production at the time of disclosure."
     },
-    "description": "Token holders temporarily unable to transfer holdings ,users spoof each other, theft of yield - Transient consensus failures. Prize will be capped to 30% of the amount that could be frozen, extracted or at risk in production at the time of disclosure."
-},
-{
-    "name": "critical",
-    "index": 20,
-    "contracts-covered": [],
-    "nft-metadata": {
-        "name": "Protocol savior",
-        "description": " critical severity reward",
-        "animation_url": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSize-Critical-Swarm%20of%20Thoughts.mp4",
-        "image": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSize-Critical-Swarm%20of%20Thoughts.mp4",
+    {
+        "name": "critical",
+        "index": 20,
+        "contracts-covered": [],
+        "nft-metadata": {
+            "name": "Protocol savior",
+            "description": " critical severity reward",
+            "animation_url": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSize-Critical-Swarm%20of%20Thoughts.mp4",
+            "image": "ipfs://ipfs/QmZAuKhEivpnrhPyvhJyPGti4fGLLYQLqvUtRtvsL8wkuC/SmallSize-Critical-Swarm%20of%20Thoughts.mp4",
 
-        "external_url": ""
-    },
-    "description": "Empty or freeze the contract's holdings (e.g. economic attacks, flash loans, reentrancy, MEV, logic errors, integer over-/under-flow), Cryptographic flaws. Prize will be capped to the amount that could be frozen, extracted or at risk in production at the time of disclosure."
-}]
+            "external_url": ""
+        },
+        "description": "Empty or freeze the contract's holdings (e.g. economic attacks, flash loans, reentrancy, MEV, logic errors, integer over-/under-flow), Cryptographic flaws. Prize will be capped to the amount that could be frozen, extracted or at risk in production at the time of disclosure."
+    }]
+}
--- src/pages/VaultEditorFormPage/types.ts
+++ src/pages/VaultEditorFormPage/types.ts
@@ -1,5 +1,42 @@
+import { ICommitteeMember, IVulnerabilitySeverity } from "types/types";
+
 export interface IContract {
     name: string;
     address: string;
     severities: string[];
 }
+export interface ISeveritiesTemplate {
+    severities: IVulnerabilitySeverity[]
+    name: string
+    indexArray: number[]
+}
+
+export interface IEditedVaultDescription {
+    "project-metadata": {
+        icon: string
+        website: string
+        name: string
+        tokenIcon: string
+        type?: string
+        endtime?: number
+        starttime?: number
+    }
+    "communication-channel": {
+        "pgp-pk": string | string[],
+    }
+    "committee": {
+        "multisig-address": string
+        "members": Array<ICommitteeMember>
+    }
+    "contracts-covered": {
+        contractAddress: string,
+        name: string,
+        severities: string[] // IVulnerabilitySeverity.name
+    }
+    "source": {
+        name: string
+        url: string
+    }
+    "additional-vaults"?: string[];
+}
+
