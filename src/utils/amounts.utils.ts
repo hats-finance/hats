ec0a3366ff43d1cf86a563081feff0ce6abd62be
Carlos Fontes <fonstack@gmail.com>
2022-11-04 15:39:08 +0000

    amount utils

---
+++ src/utils/amounts.utils.ts
@@ -0,0 +1,27 @@
+import { BigNumber } from "ethers";
+import { formatUnits } from "@ethersproject/units";
+
+export class Amount {
+  amount: BigNumber | undefined;
+  decimals: string;
+  symbol: string | undefined;
+
+  constructor(amount: BigNumber | undefined, decimals: string, symbol?: string) {
+    this.amount = amount;
+    this.decimals = decimals;
+    this.symbol = symbol;
+  }
+
+  get bigNumber() {
+    return this.amount ? this.amount : BigNumber.from(0);
+  }
+
+  get number() {
+    return this.amount ? Number(formatUnits(this.amount, this.decimals)) : 0;
+  }
+
+  get formatted() {
+    const symbol = this.symbol ? ` ${this.symbol}` : "";
+    return `${this.amount ? formatUnits(this.amount, this.decimals) : "-"}${symbol}`;
+  }
+}
