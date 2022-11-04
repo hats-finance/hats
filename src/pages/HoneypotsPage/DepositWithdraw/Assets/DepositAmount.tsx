c1c851a198f013a8142ba5cc2e46306b0bfb36a6
Carlos Fontes <fonstack@gmail.com>
2022-11-04 15:40:48 +0000

    use new hook created on DepositAmount component

--- src/pages/HoneypotsPage/DepositWithdraw/Assets/DepositAmount.tsx
+++ src/pages/HoneypotsPage/DepositWithdraw/Assets/DepositAmount.tsx
@@ -1,5 +1,4 @@
 import { formatUnits } from "@ethersproject/units";
-import { useEthers } from "@usedapp/core";
 import { useUserSharesPerVault } from "hooks/contractHooksCalls";
 import { IVault } from "types/types";
 
@@ -8,8 +7,7 @@ interface IProps {
 }
 
 export const DepositAmount = ({ vault }: IProps) => {
-  const { account } = useEthers();
-  const availableToWithdraw = useUserSharesPerVault(vault.master.address, vault, account!);
+  const availableToWithdraw = useUserSharesPerVault(vault);
   const formatAvailableToWithdraw = availableToWithdraw ? formatUnits(availableToWithdraw, vault.stakingTokenDecimals) : "-";
 
   return <span>{formatAvailableToWithdraw}</span>;
