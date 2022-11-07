ede8cb972163a3197fb00bc55c27a18854486fc7
Carlos Fontes <fonstack@gmail.com>
2022-11-07 12:12:01 +0000

    solved division by zero issue

--- src/pages/HoneypotsPage/DepositWithdraw/utils.ts
+++ src/pages/HoneypotsPage/DepositWithdraw/utils.ts
@@ -9,8 +9,8 @@ export const calculateWithdrawSharesValue = (
   totalUserShares?: BigNumber
 ): BigNumber | undefined => {
   if (!honeyPotBalance || !totalUserShares || !userShares) return undefined;
-  return userShares.mul(honeyPotBalance)
-    .div(totalUserShares);
+  if (totalUserShares.eq(0)) return BigNumber.from(0);
+  return userShares.mul(honeyPotBalance).div(totalUserShares);
 };
 
 /**
@@ -22,6 +22,6 @@ export const calculateSharesFromTokenAmount = (
   totalUserShares?: BigNumber
 ): BigNumber | undefined => {
   if (!honeyPotBalance || !totalUserShares || !userInputTokens) return undefined;
-  return userInputTokens.mul(totalUserShares)
-    .div(honeyPotBalance);
+  if (honeyPotBalance.eq(0)) return BigNumber.from(0);
+  return userInputTokens.mul(totalUserShares).div(honeyPotBalance);
 };
