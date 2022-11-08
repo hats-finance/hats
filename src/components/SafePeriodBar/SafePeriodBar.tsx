18ca4c0027fc2d3a441b6d6401c9442f52b5e423
Yehonatan Levi <leviyehonatan@gmail.com>
2022-11-08 16:41:21 +0200

    safe period fix

--- src/components/SafePeriodBar/SafePeriodBar.tsx
+++ src/components/SafePeriodBar/SafePeriodBar.tsx
@@ -10,25 +10,25 @@ export function SafePeriodBar() {
   const { safetyEndsAt, safetyStartsAt, isSafetyPeriod } = withdrawSafetyPeriod ?? {};
   const safetyPeriodDate = new Date(isSafetyPeriod && safetyEndsAt ? safetyEndsAt : safetyStartsAt || "").toLocaleTimeString();
 
+  if (!withdrawSafetyPeriod) return null;
 
   return (
-    withdrawSafetyPeriod ? (
-      <tr>
-        <th colSpan={7} className={classNames("safe-period", { "on": isSafetyPeriod })}>
-          <div className="text-wrapper">
-            {isSafetyPeriod ?
-              <div>{`WITHDRAWAL SAFE PERIOD IS ON UNTIL ${safetyPeriodDate}`}</div> :
-              <div>{`THE NEXT SAFE PERIOD WILL START AT ${safetyPeriodDate}`}</div>}
-            <Tooltip
-              overlayClassName="tooltip"
-              overlayInnerStyle={RC_TOOLTIP_OVERLAY_INNER_STYLE}
-              // TODO: this is true only for mainnet, should we parse the values and present them?
-              overlay="Safe period - twice a day and for 1 hour the committee gathers. During that time withdraw is disabled">
-              <div style={{ display: "flex", marginLeft: "10px" }}>
-                <InfoIcon fill={isSafetyPeriod ? Colors.darkBlue : Colors.turquoise} /></div>
-            </Tooltip>
-          </div>
-        </th>
-      </tr>
-    ) : <></>)
+    <tr>
+      <th colSpan={7} className={classNames("safe-period", { "on": isSafetyPeriod })}>
+        <div className="text-wrapper">
+          {isSafetyPeriod ?
+            <div>{`WITHDRAWAL SAFE PERIOD IS ON UNTIL ${safetyPeriodDate}`}</div> :
+            <div>{`THE NEXT SAFE PERIOD WILL START AT ${safetyPeriodDate}`}</div>}
+          <Tooltip
+            overlayClassName="tooltip"
+            overlayInnerStyle={RC_TOOLTIP_OVERLAY_INNER_STYLE}
+            // TODO: this is true only for mainnet, should we parse the values and present them?
+            overlay="Safe period - twice a day and for 1 hour the committee gathers. During that time withdraw is disabled">
+            <div style={{ display: "flex", marginLeft: "10px" }}>
+              <InfoIcon fill={isSafetyPeriod ? Colors.darkBlue : Colors.turquoise} /></div>
+          </Tooltip>
+        </div>
+      </th>
+    </tr>
+  )
 }
