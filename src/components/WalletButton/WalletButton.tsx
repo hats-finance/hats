96f17268dfe18a148fb3a32d974b7df9e63a4838
Carlos Fontes <fonstack@gmail.com>
2022-11-22 18:20:07 +0000

    removed web3modal implementation

--- src/components/WalletButton/WalletButton.tsx
+++ src/components/WalletButton/WalletButton.tsx
@@ -1,13 +1,29 @@
 import { shortenIfAddress, useEthers, useTransactions } from "@usedapp/core";
-import { useWeb3Modal } from "hooks/useWeb3Modal";
+// import { useWeb3Modal } from "hooks/useWeb3Modal";
 import { Colors } from "../../constants/constants";
 import { Dot } from "components";
 import { StyledWalletButton } from "./styles";
+import { useEffect, useState } from "react";
 
 const WalletButton = () => {
-  const { account } = useEthers();
-  const { activateProvider, deactivateProvider } = useWeb3Modal();
+  const [canReconnect, setCanReconnect] = useState(true);
+  const { account, activateBrowserWallet, deactivate } = useEthers();
+  // const { activateProvider, deactivateProvider } = useWeb3Modal();
   const currentTransaction = useTransactions().transactions.find((tx) => !tx.receipt);
+
+  useEffect(() => {
+    if (!account && canReconnect) activateBrowserWallet();
+  }, [account, canReconnect, activateBrowserWallet]);
+
+  const activateAccount = () => {
+    activateBrowserWallet();
+    setCanReconnect(true);
+  };
+
+  const deactivateAccount = () => {
+    deactivate();
+    setCanReconnect(false);
+  };
 
   const getButtonTitle = () => {
     if (account) {
@@ -23,7 +39,7 @@ const WalletButton = () => {
   };
 
   return (
-    <StyledWalletButton onClick={account ? deactivateProvider : activateProvider} existsPendingTransaction={!!currentTransaction}>
+    <StyledWalletButton onClick={account ? deactivateAccount : activateAccount} existsPendingTransaction={!!currentTransaction}>
       <Dot color={account ? Colors.turquoise : Colors.red} />
       {getButtonTitle()}
     </StyledWalletButton>
