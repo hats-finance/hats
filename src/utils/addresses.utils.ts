a18c94b45338892cb9480a168c7c320a58461114
Carlos Fontes <fonstack@gmail.com>
2022-11-24 15:08:15 +0000

    addresses utils

---
+++ src/utils/addresses.utils.ts
@@ -0,0 +1,17 @@
+import { utils } from "ethers";
+
+export const shortenIfAddress = (address: string): string => {
+  if (typeof address === "string" && address.length > 0) {
+    return shortenAddress(address);
+  }
+  return "";
+};
+
+export const shortenAddress = (address: string): string => {
+  try {
+    const str = utils.getAddress(address);
+    return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+  } catch {
+    throw new TypeError("Invalid input, address can't be parsed");
+  }
+};
