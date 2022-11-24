b9b9adf5608ab5e3186d7b58c374a08ce150df79
Carlos Fontes <fonstack@gmail.com>
2022-11-24 12:32:25 +0000

    added INFURA_API_KEY to constants

--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,8 @@
 import { VaultService, DefaultBotAddress, StagingBotAddress } from "./constants/constants";
 import { Chain, ChainId, Mainnet } from "@usedapp/core";
-import { Chains } from "constants/chains";
+import { ChainsConfig } from "config/chains";
 
-export const CHAINS = Chains;
+export const CHAINS = ChainsConfig;
 const prodHosts = ["app.hats.finance"];
 
 export const CHAINID: ChainId = process.env.REACT_APP_CHAINID
@@ -16,5 +16,6 @@ if (process.env.REACT_APP_ENDPOINT_MAINNET) {
 export const VAULT_SERVICE = process.env.REACT_APP_VAULT_SERVICE || VaultService;
 export const IS_PROD = prodHosts.includes(window.location.hostname);
 export const DEFAULT_BOT = process.env.REACT_APP_DEFAULT_BOT || (!IS_PROD && StagingBotAddress) || DefaultBotAddress;
+export const INFURA_API_KEY = process.env.REACT_APP_INFURA_API_KEY ?? "";
 
-export const defaultChain: Chain = Chains[CHAINID].chain;
+export const defaultChain: Chain = ChainsConfig[CHAINID].chain;
