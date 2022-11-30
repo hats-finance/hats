38d03fd99642a8bd2c19e9e8119b6f576340bd4f
Carlos Fontes <fonstack@gmail.com>
2022-11-30 15:04:29 +0000

    changed name from metaMask to metamask

--- src/components/WalletButton/connector.icons.ts
+++ src/components/WalletButton/connector.icons.ts
@@ -1,4 +1,4 @@
-import metaMask from "assets/icons/connectors/metaMask.png";
+import metaMask from "assets/icons/connectors/metamask.png";
 import coinbaseWallet from "assets/icons/connectors/coinbaseWallet.png";
 import walletConnect from "assets/icons/connectors/walletConnect.png";
 import disconnect from "assets/icons/connectors/disconnect.png";
