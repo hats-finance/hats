44396f6bc0ae7b01d24a7bca9c4b2a11da594faa
Yehonatan Levi <leviyehonatan@gmail.com>
2022-12-06 15:02:27 +0200

    new nft contract for goerli

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -16,24 +16,24 @@ export const ChainsConfig: { [index: number]: IChainConfiguration } = {
     vaultsNFTContract: "0x1569Fd54478B25E3AcCf3baC3f231108D95F50C4",
     chain: chain.mainnet,
     endpoint: "https://eth-mainnet.alchemyapi.io/v2/c4ovmC7YsQq1qM0lp6h7Ao9bGX_v4JG-",
-    subgraph: "https://api.thegraph.com/subgraphs/name/hats-finance/hats",
+    subgraph: "https://api.thegraph.com/subgraphs/name/hats-finance/hats"
   },
   [chain.goerli.id]: {
-    vaultsNFTContract: "0x4bdDe617aB54C6E45b4Bf08963F008dFC5da92aD",
+    vaultsNFTContract: "0x0196EdC0b3C81B79486E5D99e7D748955EE650D3",
     chain: chain.goerli,
     endpoint: "https://eth-goerli.g.alchemy.com/v2/HMtXCk0FyIfbiNAVm4Xcgr8Eqlc5_DKd",
-    subgraph: "https://api.thegraph.com/subgraphs/name/hats-finance/goerli_v2_1",
+    subgraph: "https://api.thegraph.com/subgraphs/name/hats-finance/goerli_v2_1"
   },
   [chain.optimism.id]: {
     chain: chain.optimism,
     endpoint: "https://eth-goerli.g.alchemy.com/v2/HMtXCk0FyIfbiNAVm4Xcgr8Eqlc5_DKd",
-    subgraph: "https://api.thegraph.com/subgraphs/name/hats-finance/hats_optimism_goerli",
+    subgraph: "https://api.thegraph.com/subgraphs/name/hats-finance/hats_optimism_goerli"
   },
   [chain.optimismGoerli.id]: {
     chain: chain.optimismGoerli,
     endpoint: "https://opt-goerli.g.alchemy.com/v2/sCQtpxEWIHKdIqHjde149TqzJZHsg0JT",
-    subgraph: "https://api.thegraph.com/subgraphs/name/hats-finance/hats_optimism_goerli",
-  },
+    subgraph: "https://api.thegraph.com/subgraphs/name/hats-finance/hats_optimism_goerli"
+  }
   // ============ HARDHAT ============
   // [ChainId.Hardhat]: {
   //     vaultsNFTContract: "",
