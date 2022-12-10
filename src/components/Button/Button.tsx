58c2685361d2feccf27101b9c5f92eb409367e10
Carlos Fontes <fonstack@gmail.com>
2022-12-10 00:12:11 +0000

    new changes to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,13 +2,15 @@ import { StyledButton } from "./styles";
 
 export type ButtonProps = {
   styleType?: "filled" | "outlined" | "text";
+  size?: "normal" | "small";
+  expanded?: boolean;
   onClick?: () => void;
   children: React.ReactNode;
 };
 
-export const Button = ({ styleType = "filled", onClick, children }: ButtonProps) => {
+export const Button = ({ styleType = "filled", size = "normal", expanded, onClick, children }: ButtonProps) => {
   return (
-    <StyledButton styleType={styleType} onClick={onClick}>
+    <StyledButton styleType={styleType} size={size} expanded={expanded} onClick={onClick}>
       {children}
     </StyledButton>
   );
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -4,10 +4,23 @@ import { ButtonProps } from "./Button";
 
 type StyledButtonProps = {
   styleType: ButtonProps["styleType"];
+  size: ButtonProps["size"];
+  expanded: ButtonProps["expanded"];
 };
 
 export const StyledButton = styled.button<StyledButtonProps>(
-  ({ styleType }) => css`
+  ({ styleType, size, expanded }) => css`
+    display: block;
+    width: ${expanded ? "100%" : "fit-content"};
+    padding: ${getSpacing(1.2)} ${getSpacing(2)};
+    text-transform: uppercase;
+    font-weight: 600;
+
+    ${size === "small" &&
+    css`
+      padding: ${getSpacing(0.8)} ${getSpacing(1.4)};
+    `}
+
     ${styleType === "text" &&
     css`
       display: inline;
@@ -15,6 +28,19 @@ export const StyledButton = styled.button<StyledButtonProps>(
       padding: ${getSpacing(0)} ${getSpacing(0.5)};
       margin: 0;
       text-decoration: underline;
+      color: var(--turquoise);
+    `}
+
+    ${styleType === "filled" &&
+    css`
+      background-color: var(--turquoise);
+      color: var(--dark-blue);
+    `}
+      
+    ${styleType === "outlined" &&
+    css`
+      background-color: transparent;
+      border: 1px solid var(--turquoise);
       color: var(--turquoise);
     `}
   `
