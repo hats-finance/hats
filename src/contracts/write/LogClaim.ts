7ab1da3a883b07631c32c24547cc561338e3f47f
Carlos Fontes <fonstack@gmail.com>
2022-12-19 18:03:59 +0000

    bog improvement on vulnerability submission

--- src/contracts/write/LogClaim.ts
+++ src/contracts/write/LogClaim.ts
@@ -31,11 +31,11 @@ export class LogClaimContract {
 
     return {
       ...claim,
-      sendAsync: async (data: string) => {
+      send: async (data: string) => {
         if (!vault) return;
         await switchNetworkAndValidate(chain!.id, vault!.chainId as number);
 
-        return claim.writeAsync!({ recklesslySetUnpreparedArgs: [data] });
+        return claim.write!({ recklesslySetUnpreparedArgs: [data] });
       },
     };
   };
