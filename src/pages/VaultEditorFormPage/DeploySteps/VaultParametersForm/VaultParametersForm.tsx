78abc60802185642f10b97d83f279aeed1fede2e
Carlos Fontes <fonstack@gmail.com>
2023-01-20 18:43:06 +0000

    vault creator on chain started

---
+++ src/pages/VaultEditorFormPage/DeploySteps/VaultParametersForm/VaultParametersForm.tsx
@@ -0,0 +1,3 @@
+export const VaultParametersForm = () => {
+  return <div>VaultParametersForm</div>;
+};
--- src/pages/VaultEditorFormPage/index.ts
+++ src/pages/VaultEditorFormPage/index.ts
@@ -4,4 +4,6 @@ export { CommunicationChannelForm } from "./SetupSteps/CommunicationChannelForm/
 export { ContractsCoveredList } from "./SetupSteps/ContractsCoveredList/ContractsCoveredList";
 export { CommitteeDetailsForm } from "./SetupSteps/CommitteeDetailsForm/CommitteeDetailsForm";
 export { VulnerabilitySeveritiesList } from "./SetupSteps/VulnerabilitySeveritiesList/VulnerabilitySeveritiesList";
-export { VaultFormReview } from "./VaultFormReview/VaultFormReview";
+export { VaultAssetsList } from "./DeploySteps/VaultAssetsList/VaultAssetsList";
+export { VaultParametersForm } from "./DeploySteps/VaultParametersForm/VaultParametersForm";
+export { VaultFormReview } from "./DeploySteps/VaultFormReview/VaultFormReview";
