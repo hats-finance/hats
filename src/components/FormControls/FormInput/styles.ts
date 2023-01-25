f67ae8ed9b4a79be95ec2567132de2671bb15084
Carlos Fontes <fonstack@gmail.com>
2023-01-25 16:27:30 +0000

    error border on inputs fix when focused

--- src/components/FormControls/FormInput/styles.ts
+++ src/components/FormControls/FormInput/styles.ts
@@ -64,7 +64,7 @@ export const StyledFormInput = styled.div<StyledFormInputProps>(
 
       ${hasError &&
       css`
-        border-color: var(--error-red);
+        border-color: var(--error-red) !important;
       `}
 
       ${isCheckOrRadio &&
