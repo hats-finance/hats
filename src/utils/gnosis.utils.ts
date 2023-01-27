66fe4dfdb308df5f9be56f24d1bab5a95912500b
Carlos Fontes <fonstack@gmail.com>
2023-01-27 19:06:24 +0000

    added gnosis safe api calls

--- src/utils/gnosis.utils.ts
+++ src/utils/gnosis.utils.ts
@@ -1,17 +1,72 @@
-import { Chain } from "@wagmi/core";
+import { mainnet, goerli, optimism, arbitrum, polygon, avalanche, bsc } from "wagmi/chains";
 import axios from "axios";
 
-const getGnosisApiEndpoint = (txHash: string, chain: Chain | undefined): string => {
-  if (!chain) return "";
-  return `https://safe-transaction-${chain.network}.safe.global/api/v1/multisig-transactions/${txHash}`;
+const getGnosisChainNameByChainId = (chainId: number): string => {
+  switch (chainId) {
+    case mainnet.id:
+      return "mainnet";
+    case goerli.id:
+      return "goerli";
+    case arbitrum.id:
+      return "arbitrum";
+    case optimism.id:
+      return "optimism";
+    case polygon.id:
+      return "polygon";
+    case avalanche.id:
+      return "avalanche";
+    case bsc.id:
+      return "bsc";
+    default:
+      throw new Error(`Gnosis doesn't support chainId:${chainId} yet`);
+  }
 };
 
-export const isAGnosisSafeTx = async (tx: string, chain: Chain | undefined): Promise<boolean> => {
+const getGnosisTxsApiEndpoint = (txHash: string, chainId: number): string => {
+  if (!chainId) return "";
+  return `https://safe-transaction-${getGnosisChainNameByChainId(chainId)}.safe.global/api/v1/multisig-transactions/${txHash}`;
+};
+
+export const isAGnosisSafeTx = async (tx: string, chainId: number | undefined): Promise<boolean> => {
   try {
-    const res = await axios.get(getGnosisApiEndpoint(tx, chain));
+    if (!chainId) throw new Error("Please provide chainId");
+
+    const res = await axios.get(getGnosisTxsApiEndpoint(tx, chainId));
 
     return !!res.data?.safeTxHash;
   } catch (error) {
     return false;
+  }
+};
+
+const getGnosisSafeStatusApiEndpoint = (safeAddress: string, chainId: number): string => {
+  if (!chainId) return "";
+  return `https://safe-transaction-${getGnosisChainNameByChainId(chainId)}.safe.global/api/v1/safes/${safeAddress}`;
+};
+
+export const getGnosisSafeInfo = async (
+  address: string,
+  chainId: number | undefined
+): Promise<{ isSafeAddress: boolean; owners: string[]; threshold: number }> => {
+  try {
+    if (!chainId) throw new Error("Please provide chainId");
+
+    const res = await axios.get(getGnosisSafeStatusApiEndpoint(address, chainId));
+    const data = res.data;
+
+    if (!data) throw new Error("No data");
+
+    return {
+      isSafeAddress: true,
+      owners: data.owners,
+      threshold: data.threshold,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      isSafeAddress: false,
+      owners: [],
+      threshold: 0,
+    };
   }
 };
