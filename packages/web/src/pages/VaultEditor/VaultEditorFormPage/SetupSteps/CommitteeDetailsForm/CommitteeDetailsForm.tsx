8e30234e157da2fef7eef8a49c0302ae0c5b87ba
Carlos Fontes <fonstack@gmail.com>
2023-03-14 12:16:18 +0000

    multisig cant be edited on edititingExistingVault

--- packages/web/src/pages/VaultEditor/VaultEditorFormPage/SetupSteps/CommitteeDetailsForm/CommitteeDetailsForm.tsx
+++ packages/web/src/pages/VaultEditor/VaultEditorFormPage/SetupSteps/CommitteeDetailsForm/CommitteeDetailsForm.tsx
@@ -64,7 +64,7 @@ export function CommitteeDetailsForm() {
       <FormInput
         {...register("committee.multisig-address")}
         label={t("VaultEditor.multisig-address")}
-        disabled={!committeeChainId || allFormDisabled}
+        disabled={!committeeChainId || isEditingExitingVault || allFormDisabled}
         pastable
         colorable
         placeholder={t("VaultEditor.vault-details.multisig-address-placeholder")}
