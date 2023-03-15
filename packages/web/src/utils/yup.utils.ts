0012f95f3b4207be3400c3cf8d425daed11b4e88
Carlos Fontes <fonstack@gmail.com>
2023-03-15 10:52:49 +0000

    improved URL regex and issue with member image

--- packages/web/src/utils/yup.utils.ts
+++ packages/web/src/utils/yup.utils.ts
@@ -6,7 +6,7 @@ import { isEmailAddress } from "./emails.utils";
 import { getTokenInfo } from "./tokens.utils";
 
 function checkUrl(url: string) {
-  const urlRegex = new RegExp(/(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#()?&//=]*)/);
+  const urlRegex = new RegExp(/(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,10}\b([-a-zA-Z0-9@:%_+.~#()?&//=]*)/);
   return urlRegex.test(url);
 }
 
