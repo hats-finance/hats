a5030ae560c7699997e591d5322a3970f03f9a3d
Carlos Fontes <fonstack@gmail.com>
2023-03-21 17:53:49 +0000

    added clean object function

--- packages/web/src/components/WithTooltip/WithTooltip.tsx
+++ packages/web/src/components/WithTooltip/WithTooltip.tsx
@@ -5,11 +5,12 @@ import { RC_TOOLTIP_OVERLAY_INNER_STYLE } from "constants/constants";
 export type WithTooltipProps = {
   children: ReactElement;
   text: string;
+  placement?: "left" | "right" | "top" | "bottom";
 };
 
-export const WithTooltip = ({ children, text }: WithTooltipProps) => {
+export const WithTooltip = ({ children, text, placement }: WithTooltipProps) => {
   return (
-    <Tooltip overlayClassName="tooltip" overlayInnerStyle={RC_TOOLTIP_OVERLAY_INNER_STYLE} overlay={text}>
+    <Tooltip overlayClassName="tooltip" placement={placement} overlayInnerStyle={RC_TOOLTIP_OVERLAY_INNER_STYLE} overlay={text}>
       {children}
     </Tooltip>
   );
