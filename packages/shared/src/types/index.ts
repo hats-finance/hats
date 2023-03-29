922c0476c03c97b8954dcf0a88d40742fb4ec018
Carlos Fontes <fonstack@gmail.com>
2023-03-29 19:54:34 +0100

    new shared lib with payout models

--- packages/shared/src/types/index.ts
+++ packages/shared/src/types/index.ts
@@ -1,2 +1,3 @@
 export * from "./types";
 export * from "./editor";
+export * from "./payout";
---
+++ packages/shared/src/types/payout.ts
@@ -0,0 +1,36 @@
+export interface IPayoutResponse {
+  _id: string;
+  vaultAddress: string;
+  chainId: number;
+  payoutData: IPayoutData;
+  nonce: number;
+  txToSign: string;
+  signatures: IPayoutSignature[];
+  status: PayoutStatus;
+  lastActionNeededNotifiedAt: Date;
+  payoutTxHash: string;
+}
+
+export interface IPayoutData {
+  title: string;
+  reportTxid: string;
+  reportDate: Date;
+  encryptedMessage: string;
+  decryptedMessage: string;
+  percentageToPay: number;
+  beneficiary: string;
+}
+
+export interface IPayoutSignature {
+  signature: string;
+  signedAt: Date;
+  signerAddress: string;
+  membersNotified: boolean;
+}
+
+export enum PayoutStatus {
+  Creating,
+  Pending,
+  ReadyToExecute,
+  Executed,
+}
