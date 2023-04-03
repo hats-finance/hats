b6bcc48e486a3ff0a6e66333d8b93aed34adba7f
Carlos Fontes <fonstack@gmail.com>
2023-04-03 13:44:53 +0100

    created payout formSchema

---
+++ packages/web/src/pages/CommitteeTools/PayoutsTool/PayoutFormPage/formSchema.ts
@@ -0,0 +1,14 @@
+import { TFunction } from "react-i18next";
+import * as Yup from "yup";
+import { getTestWalletAddress, getTestNumberInBetween, getTestUrl } from "utils/yup.utils";
+
+export const getPayoutDataYupSchema = (intl: TFunction) =>
+  Yup.object().shape({
+    beneficiary: Yup.string().test(getTestWalletAddress(intl)).required(intl("required")),
+    title: Yup.string().required(intl("required")),
+    severity: Yup.string().required(intl("required")),
+    percentageToPay: Yup.number().test(getTestNumberInBetween(intl, 0, 100, true)).required(intl("required")),
+    explanation: Yup.string().required(intl("required")),
+    nftUrl: Yup.string().test(getTestUrl(intl)).required(intl("required")),
+    additionalInfo: Yup.string().required(intl("required")),
+  });
