7d0cb0bae0f6ec306430d54d4d73cd42dbcf479e
Carlos Fontes <fonstack@gmail.com>
2023-04-03 18:18:28 +0100

    added visible property to WithTooltip

--- packages/web/src/components/WithTooltip/WithTooltip.tsx
+++ packages/web/src/components/WithTooltip/WithTooltip.tsx
@@ -5,10 +5,25 @@ import { RC_TOOLTIP_OVERLAY_INNER_STYLE } from "constants/constants";
 export type WithTooltipProps = {
   children: ReactElement;
   text: string;
+  visible?: boolean;
   placement?: "left" | "right" | "top" | "bottom";
 };
 
-export const WithTooltip = ({ children, text, placement }: WithTooltipProps) => {
+export const WithTooltip = ({ children, text, placement, visible }: WithTooltipProps) => {
+  if (visible === true || visible === false) {
+    return (
+      <Tooltip
+        visible={visible}
+        overlayClassName="tooltip"
+        placement={placement}
+        overlayInnerStyle={RC_TOOLTIP_OVERLAY_INNER_STYLE}
+        overlay={text}
+      >
+        {children}
+      </Tooltip>
+    );
+  }
+
   return (
     <Tooltip overlayClassName="tooltip" placement={placement} overlayInnerStyle={RC_TOOLTIP_OVERLAY_INNER_STYLE} overlay={text}>
       {children}
