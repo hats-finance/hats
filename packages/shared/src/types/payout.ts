c4adb5c954db28d49b44c673c85dc5bcac1296cf
Carlos Fontes <fonstack@gmail.com>
2023-04-04 17:45:42 +0100

    added docs to payout status

--- packages/shared/src/types/payout.ts
+++ packages/shared/src/types/payout.ts
@@ -18,7 +18,7 @@ export interface IPayoutData {
   beneficiary: string;
   title: string;
   severity: string;
-  percentageToPay: string;
+  percentageToPay: string; // Number between 0 and 100
   explanation: string;
   nftUrl: string;
   additionalInfo: string;
@@ -36,12 +36,12 @@ export interface IPayoutSignature {
 }
 
 export enum PayoutStatus {
-  Creating = "creating",
-  Pending = "pending",
-  ReadyToExecute = "readyToExecute",
-  UnderReview = "underReview",
-  Executed = "executed",
-  Rejected = "rejected",
+  Creating = "creating", // is a draft
+  Pending = "pending", // is ready to be signed and waiting signatures
+  ReadyToExecute = "readyToExecute", // the minimum signatures are reached and the payout is ready to be executed
+  UnderReview = "underReview", // the payout is under arbitrator review
+  Executed = "executed", // the payout was executed by arbitrator
+  Rejected = "rejected", // the payout was rejected by arbitrator
 }
 
 export const payoutStatusInfo = {
