92e1866c6a1cf5338abf2bbca4d1626019520fa1
Carlos Fontes <fonstack@gmail.com>
2023-04-11 14:37:53 +0100

    added options to shortenAddress

--- packages/web/src/utils/addresses.utils.ts
+++ packages/web/src/utils/addresses.utils.ts
@@ -1,19 +1,27 @@
 import { utils } from "ethers";
 import { isAddress as isAddressEthers } from "ethers/lib/utils";
 
+type ShortenOptions = {
+  startLength?: number;
+  endLength?: number;
+};
+
 export const isAddress = isAddressEthers;
 
-export const shortenIfAddress = (address?: string): string => {
+export const shortenIfAddress = (address?: string, opts?: ShortenOptions): string => {
   if (typeof address === "string" && address.length > 0) {
-    return shortenAddress(address);
+    return shortenAddress(address, opts);
   }
   return "";
 };
 
-export const shortenAddress = (address: string): string => {
+export const shortenAddress = (address: string, opts?: ShortenOptions): string => {
+  const startLength = opts?.startLength || 4;
+  const endLength = opts?.endLength || 4;
+
   try {
     const str = utils.getAddress(address);
-    return str.substring(0, 4) + "..." + str.substring(str.length - 4);
+    return str.substring(0, startLength) + "..." + str.substring(str.length - endLength);
   } catch {
     throw new TypeError("Invalid input, address can't be parsed");
   }
