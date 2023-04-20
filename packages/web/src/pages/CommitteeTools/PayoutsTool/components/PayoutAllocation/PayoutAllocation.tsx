f70cbcdb79074d087ece6f49e311effaeeba5be6
Carlos Fontes <fonstack@gmail.com>
2023-04-20 18:19:37 +0100

    ready payout split allocation

--- packages/web/src/pages/CommitteeTools/PayoutsTool/components/PayoutAllocation/PayoutAllocation.tsx
+++ packages/web/src/pages/CommitteeTools/PayoutsTool/components/PayoutAllocation/PayoutAllocation.tsx
@@ -1,11 +1,11 @@
 import { useTranslation } from "react-i18next";
 import { IPayoutResponse, IVault, IVulnerabilitySeverity } from "@hats-finance/shared";
-import { FormInput } from "components";
-import { useVaults } from "hooks/vaults/useVaults";
+import { FormInput, WithTooltip } from "components";
 import { NftPreview } from "../NftPreview/NftPreview";
-import { calculateAmountInTokensFromPercentage } from "../../utils/calculateAmountInTokensFromPercentage";
+import { usePayoutAmountsInfo } from "../../utils/usePayoutAmountsInfo";
 import { StyledPayoutAllocation } from "./styles";
 import ArrowDownIcon from "@mui/icons-material/ArrowDownwardOutlined";
+import InfoIcon from "@mui/icons-material/InfoOutlined";
 
 type PayoutAllocationProps = {
   selectedSeverity: IVulnerabilitySeverity | undefined;
@@ -16,12 +16,13 @@ type PayoutAllocationProps = {
 
 export const PayoutAllocation = ({ vault, payout, percentageToPay, selectedSeverity }: PayoutAllocationProps) => {
   const { t } = useTranslation();
-  const { tokenPrices } = useVaults();
+
+  const { immediateAmount, vestedAmount, committeeAmount, governanceAmount, hatsRewardAmount, totalAmount } =
+    usePayoutAmountsInfo(vault, payout, percentageToPay);
+
+  console.log({ immediateAmount, vestedAmount, committeeAmount, governanceAmount, hatsRewardAmount, totalAmount });
 
   if (!vault) return null;
-
-  const amountInTokensToPay = calculateAmountInTokensFromPercentage(percentageToPay, vault, tokenPrices);
-
   return (
     <StyledPayoutAllocation>
       <div className="result-divider">
@@ -30,15 +31,82 @@ export const PayoutAllocation = ({ vault, payout, percentageToPay, selectedSever
         <div />
       </div>
 
-      <div>{t("Payouts.resultDescription")}</div>
+      <div className="mb-5">{t("Payouts.resultDescription")}</div>
       <div className="result-container">
         <NftPreview vault={vault} severityName={selectedSeverity?.name} nftData={selectedSeverity?.["nft-metadata"]} />
-        <FormInput value={amountInTokensToPay} label={t("Payouts.payoutSum")} readOnly />
+        <FormInput value={`≈ ${totalAmount?.tokens} ~ ${totalAmount?.usd}`} label={t("Payouts.totalPayoutSum")} readOnly />
       </div>
 
-      <hr />
-      <div>PayoutAllocation</div>
-      <hr />
+      <div className="allocations">
+        <div className="mb-5">
+          <strong>{t("Payouts.payoutAllocationExplanation")}</strong>
+        </div>
+        {immediateAmount && (
+          <div className="allocation">
+            <WithTooltip text={t("immediateSplitExplanation")}>
+              <div className="name">
+                {t("immediate")} <InfoIcon />
+              </div>
+            </WithTooltip>
+            <div className="values">
+              <div className="percentage">{immediateAmount.percentage}</div>
+              <div className="sum">{`≈ ${immediateAmount.tokens} ~ ${immediateAmount.usd}`}</div>
+            </div>
+          </div>
+        )}
+        {vestedAmount && (
+          <div className="allocation">
+            <WithTooltip text={t("vestedSplitExplanation")}>
+              <div className="name">
+                {t("vested")} <InfoIcon />
+              </div>
+            </WithTooltip>
+            <div className="values">
+              <div className="percentage">{vestedAmount.percentage}</div>
+              <div className="sum">{`≈ ${vestedAmount.tokens} ~ ${vestedAmount.usd}`}</div>
+            </div>
+          </div>
+        )}
+        {hatsRewardAmount && (
+          <div className="allocation">
+            <WithTooltip text={t("hatsRewardSplitExplanation")}>
+              <div className="name">
+                {t("hatsReward")} <InfoIcon />
+              </div>
+            </WithTooltip>
+            <div className="values">
+              <div className="percentage">{hatsRewardAmount.percentage}</div>
+              <div className="sum">{`≈ ${hatsRewardAmount.tokens} ~ ${hatsRewardAmount.usd}`}</div>
+            </div>
+          </div>
+        )}
+        {committeeAmount && (
+          <div className="allocation">
+            <WithTooltip text={t("committeeSplitExplanation")}>
+              <div className="name">
+                {t("committee")} <InfoIcon />
+              </div>
+            </WithTooltip>
+            <div className="values">
+              <div className="percentage">{committeeAmount.percentage}</div>
+              <div className="sum">{`≈ ${committeeAmount.tokens} ~ ${committeeAmount.usd}`}</div>
+            </div>
+          </div>
+        )}
+        {governanceAmount && (
+          <div className="allocation">
+            <WithTooltip text={t("hatsGovFee")}>
+              <div className="name">
+                {t("governance")} <InfoIcon />
+              </div>
+            </WithTooltip>
+            <div className="values">
+              <div className="percentage">{governanceAmount.percentage}</div>
+              <div className="sum">{`≈ ${governanceAmount.tokens} ~ ${governanceAmount.usd}`}</div>
+            </div>
+          </div>
+        )}
+      </div>
     </StyledPayoutAllocation>
   );
 };
--- packages/web/src/pages/CommitteeTools/PayoutsTool/components/PayoutAllocation/styles.ts
+++ packages/web/src/pages/CommitteeTools/PayoutsTool/components/PayoutAllocation/styles.ts
@@ -21,4 +21,41 @@ export const StyledPayoutAllocation = styled.div`
     gap: ${getSpacing(3)};
     margin: ${getSpacing(1)} 0 ${getSpacing(2)};
   }
+
+  .allocations {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: ${getSpacing(1.5)};
+    border-top: 1px solid var(--grey-600);
+    border-bottom: 1px solid var(--grey-600);
+    padding: ${getSpacing(2)} ${getSpacing(1)};
+    margin-top: ${getSpacing(4)};
+
+    .allocation {
+      display: flex;
+      justify-content: space-between;
+
+      .name {
+        display: flex;
+        align-items: center;
+        gap: ${getSpacing(0.5)};
+      }
+
+      .values {
+        display: flex;
+        gap: ${getSpacing(1.5)};
+
+        .percentage,
+        .sum {
+          padding: ${getSpacing(1.2)} ${getSpacing(2)};
+          border: 1px solid var(--grey-600);
+        }
+
+        .sum {
+          border-color: var(--teal);
+        }
+      }
+    }
+  }
 `;
