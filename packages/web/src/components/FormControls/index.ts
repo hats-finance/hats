d24e1f3c1b00ddead951899709b619a7d05c3c1f
Carlos Fontes <fonstack@gmail.com>
2023-04-26 16:42:52 +0100

    changed name from FormJSONFileInput to FormJSONCSVFileInput

--- packages/web/src/components/FormControls/index.ts
+++ packages/web/src/components/FormControls/index.ts
@@ -1,7 +1,7 @@
 export { FormDateInput } from "./FormDateInput/FormDateInput";
 export { FormIconInput } from "./FormIconInput/FormIconInput";
 export { FormInput } from "./FormInput/FormInput";
-export { FormJSONFileInput } from "./FormJSONFileInput/FormJSONFileInput";
+export { FormJSONCSVFileInput } from "./FormJSONCSVFileInput/FormJSONCSVFileInput";
 export { FormPgpPublicKeyInput } from "./FormPgpPublicKeyInput/FormPgpPublicKeyInput";
 export { FormRadioInput } from "./FormRadioInput/FormRadioInput";
 export { FormSelectInput, type FormSelectInputOption } from "./FormSelectInput/FormSelectInput";
