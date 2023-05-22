10e82fcbf956eaae363bcc834cc8c58fdf640b21
Carlos Fontes <fonstack@gmail.com>
2023-05-22 12:32:55 +0100

    added trycath on getRegistries shared

--- packages/shared/src/utils/registries.utils.ts
+++ packages/shared/src/utils/registries.utils.ts
@@ -21,53 +21,57 @@ type IRegistryInfo = {
 };
 
 export const getAllRegistriesInfo = async (): Promise<IRegistryInfo[]> => {
-  const subgraphsRequests = Object.values(ChainsConfig).map((chain) => {
-    return axios.post(
-      chain.subgraph,
-      JSON.stringify({
-        query: GET_REGISTRIES,
-      }),
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
+  try {
+    const subgraphsRequests = Object.values(ChainsConfig).map((chain) => {
+      return axios.post(
+        chain.subgraph,
+        JSON.stringify({
+          query: GET_REGISTRIES,
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    });
+
+    const subgraphsResponses = await Promise.all(subgraphsRequests);
+    const subgraphsData = subgraphsResponses.map((res) => res.data);
+
+    const registries: IRegistryInfo[] = [];
+    for (let i = 0; i < subgraphsData.length; i++) {
+      const chainId = Object.values(ChainsConfig)[i].chain.id;
+
+      if (!subgraphsData[i].data || !subgraphsData[i].data.masters) continue;
+
+      for (const registry of subgraphsData[i].data.masters) {
+        const withdrawPeriod = +registry.withdrawPeriod;
+        const safetyPeriod = +registry.safetyPeriod;
+        const secondsInPeriod = (Date.now() / 1000) % (withdrawPeriod + safetyPeriod);
+
+        let nextSafetyPeriod: number;
+        let isSafetyPeriod = false;
+        if (secondsInPeriod >= withdrawPeriod) {
+          nextSafetyPeriod = safetyPeriod + withdrawPeriod * 2 - secondsInPeriod;
+          isSafetyPeriod = true;
+        } else {
+          nextSafetyPeriod = withdrawPeriod - secondsInPeriod;
+        }
+
+        registries.push({
+          address: registry.address,
+          safetyPeriod,
+          withdrawPeriod,
+          nextSafetyPeriod,
+          isSafetyPeriod,
+          chainId,
+        });
       }
-    );
-  });
-
-  const subgraphsResponses = await Promise.all(subgraphsRequests);
-  const subgraphsData = subgraphsResponses.map((res) => res.data);
-
-  const registries: IRegistryInfo[] = [];
-  for (let i = 0; i < subgraphsData.length; i++) {
-    const chainId = Object.values(ChainsConfig)[i].chain.id;
-
-    if (!subgraphsData[i].data || !subgraphsData[i].data.masters) continue;
-
-    for (const registry of subgraphsData[i].data.masters) {
-      const withdrawPeriod = +registry.withdrawPeriod;
-      const safetyPeriod = +registry.safetyPeriod;
-      const secondsInPeriod = (Date.now() / 1000) % (withdrawPeriod + safetyPeriod);
-
-      let nextSafetyPeriod: number;
-      let isSafetyPeriod = false;
-      if (secondsInPeriod >= withdrawPeriod) {
-        nextSafetyPeriod = safetyPeriod + withdrawPeriod * 2 - secondsInPeriod;
-        isSafetyPeriod = true;
-      } else {
-        nextSafetyPeriod = withdrawPeriod - secondsInPeriod;
-      }
-
-      registries.push({
-        address: registry.address,
-        safetyPeriod,
-        withdrawPeriod,
-        nextSafetyPeriod,
-        isSafetyPeriod,
-        chainId,
-      });
     }
-  }
 
-  return registries;
+    return registries;
+  } catch (error) {
+    return [];
+  }
 };
