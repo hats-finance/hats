2cb0dd973529024872f39d4c7d76f2a447e5368f
Carlos Fontes <fonstack@gmail.com>
2023-05-26 12:56:02 +0100

    hide embassy notification

--- packages/web/src/layout/BasicLayout/BasicLayout.tsx
+++ packages/web/src/layout/BasicLayout/BasicLayout.tsx
@@ -1,6 +1,5 @@
 import { changeScreenSize } from "actions/index";
 import { CookiesBanner, Header } from "components";
-import EmbassyNotificationBar from "components/EmbassyNotificationBar/EmbassyNotificationBar";
 import Sidebar from "components/Sidebar/Sidebar";
 import { LocalStorage, SMALL_SCREEN_BREAKPOINT, ScreenSize } from "constants/constants";
 import { useState } from "react";
@@ -11,9 +10,9 @@ import { AppContent, AppLayout, ContentWrapper, StyledApp } from "./styles";
 const BasicLayout = (): JSX.Element => {
   const dispatch = useDispatch();
   const [acceptedCookies, setAcceptedCookies] = useState(localStorage.getItem(LocalStorage.Cookies));
-  const [hasSeenEmbassyNotification, setHasSeenEmbassyNotification] = useState(
-    localStorage.getItem(LocalStorage.EmbassyNotification)
-  );
+  // const [hasSeenEmbassyNotification, setHasSeenEmbassyNotification] = useState(
+  //   localStorage.getItem(LocalStorage.EmbassyNotification)
+  // );
 
   const screenSize = window.matchMedia(`(min-width: ${SMALL_SCREEN_BREAKPOINT})`);
   screenSize.addEventListener("change", (screenSize) => {
@@ -23,9 +22,9 @@ const BasicLayout = (): JSX.Element => {
   const getBannersAndModals = () => (
     <>
       {acceptedCookies !== "1" && <CookiesBanner onAcceptedCookies={() => setAcceptedCookies("1")} />}
-      {hasSeenEmbassyNotification !== "1" && (
+      {/* {hasSeenEmbassyNotification !== "1" && (
         <EmbassyNotificationBar setHasSeenEmbassyNotification={() => setHasSeenEmbassyNotification("1")} />
-      )}
+      )} */}
     </>
   );
 
