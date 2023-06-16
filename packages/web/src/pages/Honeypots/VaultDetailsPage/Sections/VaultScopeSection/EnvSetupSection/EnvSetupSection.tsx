4e19d531a884e2c180bf423a4dce08e3f3394902
Carlos Fontes <fonstack@gmail.com>
2023-06-16 16:51:17 +0100

    added tooling info on env instructions section

--- packages/web/src/pages/Honeypots/VaultDetailsPage/Sections/VaultScopeSection/EnvSetupSection/EnvSetupSection.tsx
+++ packages/web/src/pages/Honeypots/VaultDetailsPage/Sections/VaultScopeSection/EnvSetupSection/EnvSetupSection.tsx
@@ -1,15 +1,23 @@
 import { IVault } from "@hats-finance/shared";
 import MDEditor from "@uiw/react-md-editor";
+import { Pill } from "components";
+import { useTranslation } from "react-i18next";
 
 type EnvSetupSectionProps = {
   vault: IVault;
 };
 
 export const EnvSetupSection = ({ vault }: EnvSetupSectionProps) => {
+  const { t } = useTranslation();
+
   if (!vault.description?.scope?.protocolSetupInstructions.instructions) return null;
 
   return (
     <div className="scope-section-container">
+      <div className="flex-horizontal mb-4">
+        <span className="bold">{t("toolUsed")}:</span> <Pill text={vault.description?.scope?.protocolSetupInstructions.tooling} />
+      </div>
+
       <MDEditor.Markdown
         source={vault.description?.scope?.protocolSetupInstructions.instructions}
         style={{ whiteSpace: "normal", fontSize: "var(--xsmall)", background: "transparent" }}
