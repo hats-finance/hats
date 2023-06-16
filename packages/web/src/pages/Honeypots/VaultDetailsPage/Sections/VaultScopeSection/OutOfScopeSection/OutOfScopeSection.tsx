16db198b5285c42a5a1265a2a9d728534226889a
Carlos Fontes <fonstack@gmail.com>
2023-06-16 16:38:22 +0100

    ready outOfScope section

--- packages/web/src/pages/Honeypots/VaultDetailsPage/Sections/VaultScopeSection/OutOfScopeSection/OutOfScopeSection.tsx
+++ packages/web/src/pages/Honeypots/VaultDetailsPage/Sections/VaultScopeSection/OutOfScopeSection/OutOfScopeSection.tsx
@@ -1,4 +1,5 @@
 import { IVault } from "@hats-finance/shared";
+import MDEditor from "@uiw/react-md-editor";
 import { StyledOutOfScopeSection } from "./styles";
 
 type OutOfScopeSectionProps = {
@@ -6,5 +7,14 @@ type OutOfScopeSectionProps = {
 };
 
 export const OutOfScopeSection = ({ vault }: OutOfScopeSectionProps) => {
-  return <StyledOutOfScopeSection className="scope-section-container">OutOfScopeSection</StyledOutOfScopeSection>;
+  if (!vault.description?.scope?.outOfScope) return null;
+
+  return (
+    <StyledOutOfScopeSection className="scope-section-container">
+      <MDEditor.Markdown
+        source={vault.description.scope.outOfScope}
+        style={{ whiteSpace: "normal", fontSize: "var(--xsmall)", background: "transparent" }}
+      />
+    </StyledOutOfScopeSection>
+  );
 };
