a684517b95800cdf309f573f5db43eddeddc5b79
Carlos Fontes <fonstack@gmail.com>
2023-06-16 16:40:03 +0100

    deleted unused file

--- packages/web/src/pages/Honeypots/VaultDetailsPage/Sections/VaultScopeSection/OutOfScopeSection/OutOfScopeSection.tsx
+++ packages/web/src/pages/Honeypots/VaultDetailsPage/Sections/VaultScopeSection/OutOfScopeSection/OutOfScopeSection.tsx
@@ -1,6 +1,5 @@
 import { IVault } from "@hats-finance/shared";
 import MDEditor from "@uiw/react-md-editor";
-import { StyledOutOfScopeSection } from "./styles";
 
 type OutOfScopeSectionProps = {
   vault: IVault;
@@ -10,11 +9,11 @@ export const OutOfScopeSection = ({ vault }: OutOfScopeSectionProps) => {
   if (!vault.description?.scope?.outOfScope) return null;
 
   return (
-    <StyledOutOfScopeSection className="scope-section-container">
+    <div className="scope-section-container">
       <MDEditor.Markdown
         source={vault.description.scope.outOfScope}
         style={{ whiteSpace: "normal", fontSize: "var(--xsmall)", background: "transparent" }}
       />
-    </StyledOutOfScopeSection>
+    </div>
   );
 };
