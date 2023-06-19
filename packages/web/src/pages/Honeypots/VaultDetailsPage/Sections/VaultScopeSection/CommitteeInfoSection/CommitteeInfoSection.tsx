7cc2f9a7695458a4d9ffca5f071f18ca0cb44374
Carlos Fontes <fonstack@gmail.com>
2023-06-19 16:37:35 +0100

    advances on committe cards

--- packages/web/src/pages/Honeypots/VaultDetailsPage/Sections/VaultScopeSection/CommitteeInfoSection/CommitteeInfoSection.tsx
+++ packages/web/src/pages/Honeypots/VaultDetailsPage/Sections/VaultScopeSection/CommitteeInfoSection/CommitteeInfoSection.tsx
@@ -1,4 +1,9 @@
 import { IVault } from "@hats-finance/shared";
+import profilePlaceholder from "assets/images/profile-placeholder.jpg";
+import { CopyToClipboard } from "components";
+import { useTranslation } from "react-i18next";
+import { ipfsTransformUri } from "utils";
+import { shortenIfAddress } from "utils/addresses.utils";
 import { StyledCommitteeInfoSection } from "./styles";
 
 type CommitteeInfoSectionProps = {
@@ -6,5 +11,33 @@ type CommitteeInfoSectionProps = {
 };
 
 export const CommitteeInfoSection = ({ vault }: CommitteeInfoSectionProps) => {
-  return <StyledCommitteeInfoSection className="scope-section-container">CommitteeInfoSection</StyledCommitteeInfoSection>;
+  const { t } = useTranslation();
+
+  if (!vault.description) return null;
+
+  const committee = vault.description.committee;
+  console.log(committee);
+
+  return (
+    <StyledCommitteeInfoSection className="scope-section-container">
+      <p className="committee-address">
+        <span className="bold">{t("committeeMultisigAddress")}:</span>
+        <span>{shortenIfAddress(committee["multisig-address"], { startLength: 6, endLength: 6 })}</span>
+        <CopyToClipboard valueToCopy={committee["multisig-address"]} />
+      </p>
+
+      <div className="members">
+        {committee.members.map((member, idx) => (
+          <div className="member" key={idx}>
+            <img
+              src={member["image-ipfs-link"] ? ipfsTransformUri(member["image-ipfs-link"]) : profilePlaceholder}
+              alt={member.name}
+            />
+            <p className="bold">{member.name}</p>
+            <p>{member["twitter-link"]}</p>
+          </div>
+        ))}
+      </div>
+    </StyledCommitteeInfoSection>
+  );
 };
--- packages/web/src/pages/Honeypots/VaultDetailsPage/Sections/VaultScopeSection/CommitteeInfoSection/styles.ts
+++ packages/web/src/pages/Honeypots/VaultDetailsPage/Sections/VaultScopeSection/CommitteeInfoSection/styles.ts
@@ -1,3 +1,30 @@
 import styled from "styled-components";
+import { getSpacing } from "styles";
 
-export const StyledCommitteeInfoSection = styled.div``;
+export const StyledCommitteeInfoSection = styled.div`
+  .committee-address {
+    display: flex;
+    align-items: center;
+    gap: ${getSpacing(1)};
+  }
+
+  .members {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    margin-top: ${getSpacing(4)};
+
+    .member {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: ${getSpacing(1)};
+
+      img {
+        width: ${getSpacing(8)};
+        height: ${getSpacing(8)};
+        object-fit: cover;
+        border-radius: 50%;
+      }
+    }
+  }
+`;
