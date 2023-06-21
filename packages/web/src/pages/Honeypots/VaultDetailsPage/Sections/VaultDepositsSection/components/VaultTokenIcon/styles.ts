fba35490dcb4ec3e0967b0af8c5f1288224a35fa
Carlos Fontes <fonstack@gmail.com>
2023-06-21 12:37:03 +0100

    fixes on vault token icon

--- packages/web/src/pages/Honeypots/VaultDetailsPage/Sections/VaultDepositsSection/components/VaultTokenIcon/styles.ts
+++ packages/web/src/pages/Honeypots/VaultDetailsPage/Sections/VaultDepositsSection/components/VaultTokenIcon/styles.ts
@@ -4,27 +4,28 @@ import { getSpacing } from "styles";
 export const StyledVaultTokenIcon = styled.div`
   display: flex;
   align-items: center;
-  gap: ${getSpacing(2)};
+  gap: ${getSpacing(2.5)};
+  font-weight: 700;
 
   .images {
     position: relative;
-    width: ${getSpacing(4.5)};
-    height: ${getSpacing(4.5)};
+    width: ${getSpacing(5.5)};
+    height: ${getSpacing(5.5)};
 
     img.logo {
-      width: ${getSpacing(4.5)};
-      height: ${getSpacing(4.5)};
+      width: ${getSpacing(5.5)};
+      height: ${getSpacing(5.5)};
       object-fit: contain;
       border-radius: 500px;
     }
 
     img.chain {
-      width: ${getSpacing(2.8)};
-      height: ${getSpacing(2.8)};
+      width: ${getSpacing(3)};
+      height: ${getSpacing(3)};
       object-fit: contain;
       position: absolute;
-      bottom: -4px;
-      right: -6px;
+      bottom: -2px;
+      right: -10px;
     }
   }
 `;
