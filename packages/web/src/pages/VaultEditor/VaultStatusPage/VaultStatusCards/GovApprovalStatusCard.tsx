241c1214e87bd7c606551b069d9f32ea811d25e7
Carlos Fontes <fonstack@gmail.com>
2023-06-21 19:15:24 +0100

    changed preview on status page

--- packages/web/src/pages/VaultEditor/VaultStatusPage/VaultStatusCards/GovApprovalStatusCard.tsx
+++ packages/web/src/pages/VaultEditor/VaultStatusPage/VaultStatusCards/GovApprovalStatusCard.tsx
@@ -1,17 +1,33 @@
-import { Pill, Vault } from "components";
+import ArrowIcon from "@mui/icons-material/ArrowForwardOutlined";
+import { Button, Pill } from "components";
 import { useVaults } from "hooks/vaults/useVaults";
+import { RoutePaths } from "navigation";
+import { HoneypotsRoutePaths } from "pages/Honeypots/router";
 import { useContext } from "react";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import { slugify } from "utils/slug.utils";
 import { VaultStatusContext } from "../store";
 
 export const GovApprovalStatusCard = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
 
   const { vaultData, vaultAddress } = useContext(VaultStatusContext);
   const { allVaults } = useVaults();
   const vault = allVaults?.find((vault) => vault.id === vaultAddress);
 
   const isApprovedByGov = vaultData.isRegistered;
+  const isAudit = vault && vault.description && vault.description["project-metadata"].type === "audit";
+
+  const goToDetails = () => {
+    if (!vault) return;
+
+    const mainRoute = `${RoutePaths.vaults}/${isAudit ? HoneypotsRoutePaths.audits : HoneypotsRoutePaths.bugBounties}`;
+    const vaultSlug = slugify(vault?.description?.["project-metadata"].name ?? "");
+
+    navigate(`${mainRoute}/${vaultSlug}-${vault.id}`);
+  };
 
   return (
     <div className="status-card">
@@ -32,13 +48,9 @@ export const GovApprovalStatusCard = () => {
       )}
 
       {vault && (
-        <div className="preview-vault">
-          <table>
-            <tbody>
-              <Vault expanded={true} vault={vault} noActions />
-            </tbody>
-          </table>
-        </div>
+        <Button className="mt-5" onClick={goToDetails}>
+          {t("goToVaultDetailsPage")} <ArrowIcon className="ml-3" />
+        </Button>
       )}
     </div>
   );
