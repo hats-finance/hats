f3c29f3d20f6db30ce9a13cafb65934350f86ee3
Carlos Fontes <fonstack@gmail.com>
2023-07-06 10:36:10 +0100

    using again encrypted pass

--- packages/web/src/config/encryptedStorage.ts
+++ packages/web/src/config/encryptedStorage.ts
@@ -1,8 +1,4 @@
 import { EncryptStorage } from "encrypt-storage";
+import { ENCRYPTED_STORAGE_KEY } from "settings";
 
-// Generate a cryptographically secure random key
-const array = new Uint32Array(10);
-window.crypto.getRandomValues(array);
-const encryptionKey = Array.from(array).join("");
-
-export const encryptedStorage = new EncryptStorage(encryptionKey, { storageType: "sessionStorage" });
+export const encryptedStorage = new EncryptStorage(ENCRYPTED_STORAGE_KEY, { storageType: "sessionStorage" });
