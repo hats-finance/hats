c77d9b00f1d13d77d84e7d391b7ade09d12c9342
Carlos Fontes <fonstack@gmail.com>
2023-07-06 10:36:45 +0100

    removed keystore init from submission hook

--- packages/web/src/pages/CommitteeTools/SubmissionsTool/submissionsService.hooks.ts
+++ packages/web/src/pages/CommitteeTools/SubmissionsTool/submissionsService.hooks.ts
@@ -2,7 +2,6 @@ import { UseQueryResult, useQuery } from "@tanstack/react-query";
 import { useKeystore } from "components/Keystore";
 import { useSiweAuth } from "hooks/siwe/useSiweAuth";
 import { useVaults } from "hooks/vaults/useVaults";
-import { useEffect } from "react";
 import { useAccount } from "wagmi";
 import * as SubmissionsService from "./submissionsService.api";
 
@@ -14,11 +13,7 @@ export const useVaultSubmissionsBySiweUser = (): UseQueryResult<IVaultSubmission
   const { isAuthenticated, profileData } = useSiweAuth();
   const { address } = useAccount();
   const { allSubmissions, allVaults, vaultsReadyAllChains, submissionsReadyAllChains } = useVaults();
-  const { keystore, initKeystore } = useKeystore();
-
-  useEffect(() => {
-    if (!keystore) initKeystore();
-  }, [keystore, initKeystore]);
+  const { keystore } = useKeystore();
 
   // console.log("isAuthenticated", isAuthenticated);
   // console.log("keystore", keystore);
@@ -36,6 +31,5 @@ export const useVaultSubmissionsBySiweUser = (): UseQueryResult<IVaultSubmission
 
   return {
     ...queryResult,
-    isLoading: queryResult.isLoading,
   } as UseQueryResult<IVaultSubmission[]>;
 };
