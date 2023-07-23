8786a273e68b30b4bb494e5a4258d500b611c059
benk10 <ben.kaufman10@gmail.com>
2023-07-23 18:12:17 -0500

    Skip invalid keys

--- packages/web/src/pages/Submissions/SubmissionFormPage/encrypt.ts
+++ packages/web/src/pages/Submissions/SubmissionFormPage/encrypt.ts
@@ -5,7 +5,19 @@ const IpfsHash = require("ipfs-only-hash");
 export async function encryptWithKeys(publicKeyOrKeys: string | string[], dataToEncrypt: string) {
   let encryptionKeys: MaybeArray<Key>;
   if (Array.isArray(publicKeyOrKeys)) {
-    encryptionKeys = await Promise.all(publicKeyOrKeys.map((key) => readKey({ armoredKey: key })));
+    const encryptionKeysList = await Promise.all(publicKeyOrKeys.map((key) => readKey({ armoredKey: key })));
+    encryptionKeys = [];
+    for (let key of encryptionKeys) {
+      try {
+        await key.verifyPrimaryKey();
+        encryptionKeys.push(key);
+      } catch {
+      }
+    }
+
+    if (encryptionKeys.length == 0) {
+      alert("This vault doesn’t have any valid key, please contact hats team");
+    }
   } else {
     encryptionKeys = await readKey({ armoredKey: publicKeyOrKeys });
   }
