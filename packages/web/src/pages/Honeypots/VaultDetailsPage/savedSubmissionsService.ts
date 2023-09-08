1e180d0f92ab3adcef40ae4f92c256d6b015b30f
Carlos Fontes <fonstack@gmail.com>
2023-09-08 15:51:05 +0100

    created services and hooks for getting issues and repoName from DB

---
+++ packages/web/src/pages/Honeypots/VaultDetailsPage/savedSubmissionsService.ts
@@ -0,0 +1,43 @@
+import { axiosClient } from "config/axiosClient";
+import { BASE_SERVICE_URL } from "settings";
+import { IGithubIssue } from "./types";
+
+/**
+ * Get all saved github issues for a vault
+ */
+export async function getSavedSubmissions(vaultId: string | undefined): Promise<IGithubIssue[]> {
+  if (!vaultId) return [];
+
+  try {
+    const response = await axiosClient.get(`${BASE_SERVICE_URL}/github-repos/issues/${vaultId}`);
+    const githubIssues = response.data.githubIssues as IGithubIssue[];
+    const githubIssuesWithSeverity = githubIssues.map((issue) => {
+      const severity = issue.issueData.issueDescription.match(/(\*\*Severity:\*\* (.*)\n)/)?.[2];
+      return {
+        ...issue,
+        severity,
+      };
+    });
+
+    return githubIssuesWithSeverity;
+  } catch (error) {
+    return [];
+  }
+}
+
+/**
+ * Get the repo name created for a vault
+ */
+export async function getVaultRepoName(vaultId: string | undefined): Promise<string | undefined> {
+  if (!vaultId) return undefined;
+
+  try {
+    const response = await axiosClient.get(`${BASE_SERVICE_URL}/github-repos/repo/${vaultId}`);
+    const repoName = response.data.repoName as string | undefined;
+    console.log(response);
+
+    return repoName;
+  } catch (error) {
+    return undefined;
+  }
+}
