b824a0efbef7cd9884cd86e6135f9f1271d2c9e4
Carlos Fontes <fonstack@gmail.com>
2023-09-29 11:51:43 +0100

    advances with the decryption by hats methods

--- packages/web/src/pages/CommitteeTools/DecryptionTool/DecryptionPage/DecryptionPage.tsx
+++ packages/web/src/pages/CommitteeTools/DecryptionTool/DecryptionPage/DecryptionPage.tsx
@@ -54,12 +54,14 @@ export const DecryptionPage = () => {
 
       let decryptedPart = "";
       let encryptedPart = "";
+      // let isDecryptedPartEncryptedByHats = false;
 
-      // TODO: private audits (We need to check if the message is encrypted by HATS, if so, decrypt it with backend)
+      // TODO: private audits V2 (We need to check if the message is encrypted by HATS, if so, decrypt it with backend)
       try {
         const messageObject = JSON.parse(dataToUse.encryptedMessage) as ISubmissionMessageObject;
         decryptedPart = messageObject.decrypted ?? "";
         encryptedPart = messageObject.encrypted ?? "";
+        // isDecryptedPartEncryptedByHats = messageObject.isEncryptedByHats ?? false;
       } catch (error) {
         encryptedPart = dataToUse.encryptedMessage;
       }
@@ -74,6 +76,13 @@ export const DecryptionPage = () => {
             message,
             decryptionKeys: privateKey,
           });
+
+          // TODO: private audits V2
+          // // If user could decrypt encrypted part (with committee key), lets decrypt the encryptedByHats part
+          // if (isDecryptedPartEncryptedByHats) {
+          //   const decryptedPartByHats = await decryptUsingHatsKey(decryptedPart);
+          //   decryptedPart = decryptedPartByHats;
+          // }
 
           const decryptedMessage = (decrypted as string) + (decryptedPart ? `\n\n${decryptedPart}` : "");
 
---
+++ packages/web/src/pages/CommitteeTools/DecryptionTool/DecryptionPage/decryptionService.api.ts
@@ -0,0 +1,3 @@
+export const decryptUsingHatsKey = async (encryptedData: string): Promise<string> => {
+  throw new Error("Not implemented");
+};
