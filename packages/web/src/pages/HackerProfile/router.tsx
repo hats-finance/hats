4461f88b3d86baf17eb5292a0301764ca4423b5b
Carlos Fontes <fonstack@gmail.com>
2023-10-27 15:22:31 +0100

    new profile router

---
+++ packages/web/src/pages/HackerProfile/router.tsx
@@ -0,0 +1,17 @@
+import { RoutePaths } from "navigation";
+import { Navigate, RouteObject } from "react-router-dom";
+import { HackerProfilePage } from "./HackerProfilePage/HackerProfilePage";
+
+export const hackerProfileRouter = (): RouteObject => ({
+  path: `${RoutePaths.profile}`,
+  children: [
+    {
+      path: "",
+      element: <Navigate to="/" replace={true} />,
+    },
+    {
+      path: "/:username",
+      element: <HackerProfilePage />,
+    },
+  ],
+});
