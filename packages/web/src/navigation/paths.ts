2325ca25aa75fcb8782a31fc82c8a5ede9d7c211
Carlos Fontes <fonstack@gmail.com>
2023-10-27 15:22:24 +0100

    added new profile route

--- packages/web/src/navigation/paths.ts
+++ packages/web/src/navigation/paths.ts
@@ -4,6 +4,7 @@ enum RoutePaths {
   vulnerability = "/vulnerability",
   vault_editor = "/vault-editor",
   committee_tools = "/committee-tools",
+  profile = "/profile",
   payouts = "/committee-tools/payouts",
   submissions = "/committee-tools/submissions",
   airdrop_machine = "/airdrop_machine",
