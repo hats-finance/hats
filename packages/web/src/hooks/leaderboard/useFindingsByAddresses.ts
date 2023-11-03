0f34a7582496e80ba407fc3da9bb8a6613f8218c
Carlos Fontes <fonstack@gmail.com>
2023-11-03 18:48:48 +0000

    Add useFindingsByAddresses hook for profile

---
+++ packages/web/src/hooks/leaderboard/useFindingsByAddresses.ts
@@ -0,0 +1,25 @@
+import { useSubmissions } from "hooks/subgraph/submissions/useSubmissions";
+import { useMemo } from "react";
+
+/**
+ * Returns all the findings (submissions) executed on the dApp
+ */
+export const useFindingsByAddresses = (addresses: string[] = []) => {
+  const { allSubmissionsOnEnv } = useSubmissions();
+  const addressesToUse = useMemo(() => addresses.map((a) => a.toLowerCase()), [addresses]);
+
+  const validFindings = useMemo(() => {
+    if (!allSubmissionsOnEnv) return [];
+    const findings = allSubmissionsOnEnv.filter((finding) => finding.submissionData);
+
+    const findingsByAddresses = findings.filter((finding) => {
+      if (!finding.submissionData) return false;
+      return addressesToUse.includes(finding.submitter.toLowerCase());
+    });
+
+    return findingsByAddresses;
+  }, [allSubmissionsOnEnv, addressesToUse]);
+
+  validFindings.sort((a, b) => (a.createdAt > b.createdAt ? 1 : -1));
+  return validFindings;
+};
