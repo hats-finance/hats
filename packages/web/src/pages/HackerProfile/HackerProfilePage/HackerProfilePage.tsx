cfc150e42ed82b6c1ac1997deea7f04a221dccfa
Carlos Fontes <fonstack@gmail.com>
2023-11-14 11:01:03 +0000

    added seo title to hacker profile

--- packages/web/src/pages/HackerProfile/HackerProfilePage/HackerProfilePage.tsx
+++ packages/web/src/pages/HackerProfile/HackerProfilePage/HackerProfilePage.tsx
@@ -3,7 +3,7 @@ import ArrowIcon from "@mui/icons-material/ArrowBackOutlined";
 import UnlinkIcon from "@mui/icons-material/LinkOffOutlined";
 import GitHubIcon from "assets/icons/social/github.icon";
 import TwitterIcon from "assets/icons/social/twitter.icon";
-import { Alert, Button, HackerProfileImage, Loading, Pill } from "components";
+import { Alert, Button, HackerProfileImage, Loading, Pill, Seo } from "components";
 import { queryClient } from "config/reactQuery";
 import { defaultAnchorProps } from "constants/defaultAnchorProps";
 import { getSeveritiesColorsArray } from "hooks/severities/useSeverityRewardInfo";
@@ -126,99 +126,104 @@ export const HackerProfilePage = () => {
   };
 
   return (
-    <StyledHackerProfilePage className="content-wrapper" unlinkDisabled={profileFound?.addresses.length === 1}>
-      {profileFound && (
-        <>
-          <div className="header">
-            <div className="profile-card">
-              <HackerProfileImage noMargin hackerProfile={profileFound} size="large" />
-              <div className="description">
-                <h2>{profileFound.username}</h2>
-                {profileFound.title && <p className="hacker-title">{profileFound.title}</p>}
-                {profileStats.firstSubmissionDate && (
-                  <p className="hacker-date mt-2">
-                    {t("HackerProfile.firstSubmission", { date: moment(profileStats.firstSubmissionDate).format("D MMM YYYY") })}
-                  </p>
-                )}
+    <>
+      <Seo title={t("seo.hackerProfileTitle", { username: profileFound?.username })} />
+      <StyledHackerProfilePage className="content-wrapper" unlinkDisabled={profileFound?.addresses.length === 1}>
+        {profileFound && (
+          <>
+            <div className="header">
+              <div className="profile-card">
+                <HackerProfileImage noMargin hackerProfile={profileFound} size="large" />
+                <div className="description">
+                  <h2>{profileFound.username}</h2>
+                  {profileFound.title && <p className="hacker-title">{profileFound.title}</p>}
+                  {profileStats.firstSubmissionDate && (
+                    <p className="hacker-date mt-2">
+                      {t("HackerProfile.firstSubmission", {
+                        date: moment(profileStats.firstSubmissionDate).format("D MMM YYYY"),
+                      })}
+                    </p>
+                  )}
+                </div>
+
+                <div className="socials">
+                  {profileFound.twitter_username && (
+                    <a href={`https://twitter.com/${profileFound.twitter_username}`} {...defaultAnchorProps}>
+                      <TwitterIcon />
+                    </a>
+                  )}
+                  {profileFound.github_username && (
+                    <a href={`https://github.com/${profileFound.github_username}`} {...defaultAnchorProps}>
+                      <GitHubIcon />
+                    </a>
+                  )}
+                </div>
               </div>
 
-              <div className="socials">
-                {profileFound.twitter_username && (
-                  <a href={`https://twitter.com/${profileFound.twitter_username}`} {...defaultAnchorProps}>
-                    <TwitterIcon />
-                  </a>
-                )}
-                {profileFound.github_username && (
-                  <a href={`https://github.com/${profileFound.github_username}`} {...defaultAnchorProps}>
-                    <GitHubIcon />
-                  </a>
-                )}
+              {(unlinkAddress.error || linkNewAddress.error) && (
+                <Alert className="mb-3" type="error" content={unlinkAddress.error ?? linkNewAddress.error ?? ""} />
+              )}
+
+              {isProfileOwner && (
+                <div className="actions">
+                  <div className="buttons">
+                    <Button onClick={handleStartLinkNewAddress}>
+                      <LinkIcon className="mr-2" />
+                      {t("HackerProfile.linkNewAddress")}
+                    </Button>
+                  </div>
+
+                  <div className="linked-addresses">
+                    <h3>{t("HackerProfile.linkedAddresses")}:</h3>
+                    <div className="addresses-list">
+                      {profileFound.addresses.map((address, idx) => (
+                        <div className="address" key={address} onClick={() => handleUnlinkAddress(address)}>
+                          <Pill text={shortenIfAddress(address, { startLength: 6 })} />
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+
+            <div className="stats-container">
+              {profileFound.bio && (
+                <div className="about">
+                  <h3>{t("HackerProfile.about")}</h3>
+                  <p>{profileFound.bio}</p>
+                </div>
+              )}
+              <div className="findings">
+                <div className="total-rewards">
+                  <h3>{t("HackerProfile.stats")}</h3>
+                  {profileStats.totalRewardsInUsd > 0 && (
+                    <div className="totalPrizes">
+                      <p>{`~$${formatNumber(profileStats.totalRewardsInUsd, 2)}`}</p>
+                      <span>{t("HackerProfile.earnedAtHatsFinance")}</span>
+                    </div>
+                  )}
+                </div>
+                <div className="findings-list">
+                  {profileStats.findingsGlobalStats.length === 0 && (
+                    <Alert type="info" content={t("HackerProfile.youHaveNoStats")} />
+                  )}
+                  {profileStats.findingsGlobalStats.map((severity, idx) => (
+                    <div className="stat" key={severity.severity}>
+                      <p className="count">{severity.count}</p>
+                      <Pill textColor={severityColors[idx]} isSeverity text={severity.severity} />
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
 
-            {(unlinkAddress.error || linkNewAddress.error) && (
-              <Alert className="mb-3" type="error" content={unlinkAddress.error ?? linkNewAddress.error ?? ""} />
-            )}
+            {profileStats.hackerRewardStats.length > 0 && <HackerActivity activity={profileStats.hackerRewardStats} />}
+          </>
+        )}
 
-            {isProfileOwner && (
-              <div className="actions">
-                <div className="buttons">
-                  <Button onClick={handleStartLinkNewAddress}>
-                    <LinkIcon className="mr-2" />
-                    {t("HackerProfile.linkNewAddress")}
-                  </Button>
-                </div>
-
-                <div className="linked-addresses">
-                  <h3>{t("HackerProfile.linkedAddresses")}:</h3>
-                  <div className="addresses-list">
-                    {profileFound.addresses.map((address, idx) => (
-                      <div className="address" key={address} onClick={() => handleUnlinkAddress(address)}>
-                        <Pill text={shortenIfAddress(address, { startLength: 6 })} />
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            )}
-          </div>
-
-          <div className="stats-container">
-            {profileFound.bio && (
-              <div className="about">
-                <h3>{t("HackerProfile.about")}</h3>
-                <p>{profileFound.bio}</p>
-              </div>
-            )}
-            <div className="findings">
-              <div className="total-rewards">
-                <h3>{t("HackerProfile.stats")}</h3>
-                {profileStats.totalRewardsInUsd > 0 && (
-                  <div className="totalPrizes">
-                    <p>{`~$${formatNumber(profileStats.totalRewardsInUsd, 2)}`}</p>
-                    <span>{t("HackerProfile.earnedAtHatsFinance")}</span>
-                  </div>
-                )}
-              </div>
-              <div className="findings-list">
-                {profileStats.findingsGlobalStats.length === 0 && (
-                  <Alert type="info" content={t("HackerProfile.youHaveNoStats")} />
-                )}
-                {profileStats.findingsGlobalStats.map((severity, idx) => (
-                  <div className="stat" key={severity.severity}>
-                    <p className="count">{severity.count}</p>
-                    <Pill textColor={severityColors[idx]} isSeverity text={severity.severity} />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-
-          {profileStats.hackerRewardStats.length > 0 && <HackerActivity activity={profileStats.hackerRewardStats} />}
-        </>
-      )}
-
-      {isLoadingProfile && <Loading extraText={`${t("HackerProfile.loadingProfile")}...`} />}
-    </StyledHackerProfilePage>
+        {isLoadingProfile && <Loading extraText={`${t("HackerProfile.loadingProfile")}...`} />}
+      </StyledHackerProfilePage>
+    </>
   );
 };
