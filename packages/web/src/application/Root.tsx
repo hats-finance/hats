0bd9b9b51e894ae9625159969243eec47930ae53
Carlos Fontes <fonstack@gmail.com>
2023-11-23 12:32:04 +0000

    moved db init to index

--- packages/web/src/application/Root.tsx
+++ packages/web/src/application/Root.tsx
@@ -3,7 +3,6 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Seo } from "components";
 import { KeystoreProvider } from "components/Keystore";
 import { NotificationProvider } from "components/Notifications/NotificationProvider";
-import { DBConfig } from "config/DBConfig";
 import { queryClient } from "config/reactQuery";
 import { theme } from "config/theme";
 import { wagmiClient } from "config/wagmi";
@@ -15,15 +14,12 @@ import "i18n.ts";
 import { useEffect } from "react";
 import HttpsRedirect from "react-https-redirect";
 import { useTranslation } from "react-i18next";
-import { initDB } from "react-indexed-db-hook";
 import { Provider } from "react-redux";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import { GlobalStyle } from "styles";
 import { WagmiConfig } from "wagmi";
 import store from "../store";
 import App from "./App";
-
-initDB(DBConfig);
 
 function Root() {
   const { i18n } = useTranslation();
