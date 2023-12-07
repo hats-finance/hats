6bd226a3e3f23b018887ed6abd6f09df647188bf
Carlos Fontes <fonstack@gmail.com>
2023-12-07 11:12:50 +0000

    improved APY calculation

--- packages/web/src/hooks/vaults/useVaultApy.ts
+++ packages/web/src/hooks/vaults/useVaultApy.ts
@@ -1,7 +1,9 @@
+import { Provider } from "@ethersproject/providers";
 import { formatUnits } from "@ethersproject/units";
 import { IRewardController, IVault } from "@hats-finance/shared";
 import { BigNumber } from "ethers";
-import { useBlockNumber } from "wagmi";
+import { useEffect, useState } from "react";
+import { useBlockNumber, useProvider } from "wagmi";
 
 type IVaultApy = {
   rewardController: IRewardController;
@@ -9,46 +11,59 @@ type IVaultApy = {
 };
 
 export const useVaultApy = (vault?: IVault): IVaultApy[] => {
-  const { data: blockNumber, isError, isLoading } = useBlockNumber({ chainId: vault?.chainId, watch: false });
+  const provider = useProvider();
+  const [vaultApys, setVaultApys] = useState<IVaultApy[]>([]);
+  const { data: blockNumber } = useBlockNumber({ chainId: vault?.chainId, watch: false });
 
-  if (isError || isLoading || !blockNumber) return [];
-  if (!vault) return [];
-  if (vault.version === "v1") return [];
+  useEffect(() => {
+    if (!blockNumber) return setVaultApys([]);
+    if (!vault) return setVaultApys([]);
+    if (vault.version === "v1") return setVaultApys([]);
 
-  const rewardControllers = vault.rewardControllers || [];
-  if (!rewardControllers || rewardControllers.length === 0) return [];
+    const rewardControllers = vault.rewardControllers || [];
+    if (!rewardControllers || rewardControllers.length === 0) return setVaultApys([]);
 
-  const allocPoints = vault.allocPoints || [];
-  if (!allocPoints || allocPoints.length === 0) return [];
+    const allocPoints = vault.allocPoints || [];
+    if (!allocPoints || allocPoints.length === 0) return setVaultApys([]);
 
-  const vaultApys: IVaultApy[] = [];
-  for (const [idx, controller] of rewardControllers.entries()) {
-    if (!controller) continue;
-    const allocPoint = +allocPoints[idx] ?? 0;
+    const computeApy = async () => {
+      const apys: IVaultApy[] = [];
 
-    const blocksPassed = blockNumber - +controller.startBlock;
-    const epochsPassed = Math.floor(blocksPassed / +controller.epochLength);
-    const epochRewardPerBlockBigNumber =
-      +controller.epochRewardPerBlock[
-        epochsPassed > controller.epochRewardPerBlock.length - 1 ? controller.epochRewardPerBlock.length - 1 : epochsPassed
-      ] ?? 0;
-    const epochRewardPerBlock = +formatUnits(BigNumber.from(`${epochRewardPerBlockBigNumber}`), controller.rewardTokenDecimals);
+      for (const [idx, controller] of rewardControllers.entries()) {
+        if (!controller) continue;
 
-    const apy = calculateAPY(
-      allocPoint,
-      epochRewardPerBlock,
-      +controller.totalAllocPoint,
-      +controller.epochLength * 12,
-      vault.amountsInfo?.tokenPriceUsd && controller.tokenPriceUsd
-        ? controller.tokenPriceUsd / vault.amountsInfo.tokenPriceUsd
-        : 0,
-      vault.amountsInfo?.depositedAmount ? +vault.amountsInfo.depositedAmount.tokens : 0
-    );
+        const blocksPerYear = await calculateBlocksPerYear(controller, provider);
+        const allocPoint = +allocPoints[idx] ?? 0;
+        const blocksPassed = blockNumber - +controller.startBlock;
+        const epochsPassed = Math.floor(blocksPassed / +controller.epochLength);
+        const epochRewardPerBlockBigNumber =
+          +controller.epochRewardPerBlock[
+            epochsPassed > controller.epochRewardPerBlock.length - 1 ? controller.epochRewardPerBlock.length - 1 : epochsPassed
+          ] ?? 0;
+        const epochRewardPerBlock = +formatUnits(
+          BigNumber.from(`${epochRewardPerBlockBigNumber}`),
+          controller.rewardTokenDecimals
+        );
 
-    vaultApys.push({ rewardController: controller, apy });
-  }
+        const apy = calculateAPY(
+          allocPoint,
+          epochRewardPerBlock,
+          +controller.totalAllocPoint,
+          blocksPerYear,
+          vault.amountsInfo?.tokenPriceUsd && controller.tokenPriceUsd
+            ? controller.tokenPriceUsd / vault.amountsInfo.tokenPriceUsd
+            : 0,
+          vault.amountsInfo?.depositedAmount ? +vault.amountsInfo.depositedAmount.tokens : 0
+        );
 
-  console.log(vaultApys);
+        apys.push({ rewardController: controller, apy });
+      }
+
+      setVaultApys(apys);
+    };
+    computeApy();
+  }, [blockNumber, provider, vault]);
+
   return vaultApys;
 };
 
@@ -75,4 +90,15 @@ function calculateAPY(
   const apy = (annualRewardValue / vaultTokenStaked) * 100;
 
   return apy > 100 ? +apy.toFixed(0) : +apy.toFixed(2);
+}
+
+async function calculateBlocksPerYear(controller: IRewardController, provider: Provider): Promise<number> {
+  const startedBlock = await provider.getBlock(+controller.startBlock);
+  const currentBlock = await provider.getBlock("latest");
+
+  const blocksPassed = currentBlock.number - startedBlock.number;
+  const blocksPerSecond = blocksPassed / (currentBlock.timestamp - startedBlock.timestamp);
+  const blocksPerYear = blocksPerSecond * 60 * 60 * 24 * 365;
+
+  return blocksPerYear;
 }
