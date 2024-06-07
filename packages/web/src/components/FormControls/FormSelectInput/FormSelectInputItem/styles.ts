2e47d66180f23a3587748dea084ced7e80baf04b
Carlos Fontes <fonstack@gmail.com>
2024-06-07 10:58:22 +0100

    UI changes on select form input

--- packages/web/src/components/FormControls/FormSelectInput/FormSelectInputItem/styles.ts
+++ packages/web/src/components/FormControls/FormSelectInput/FormSelectInputItem/styles.ts
@@ -1,63 +1,65 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { getSpacing } from "styles";
 
-export const StyledFormSelectInputItem = styled.label`
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: ${getSpacing(1)};
-  padding: ${getSpacing(2)};
-  user-select: none;
-  color: var(--white);
-  font-size: var(--xsmall);
-
-  &:hover {
-    background-color: var(--background-2);
-  }
-
-  .info {
-    width: calc(100% - ${getSpacing(5)});
+export const StyledFormSelectInputItem = styled.label<{ noSelectedMark: boolean }>(
+  ({ noSelectedMark }) => css`
+    cursor: pointer;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     gap: ${getSpacing(1)};
-    text-transform: capitalize;
+    padding: ${getSpacing(2)};
+    user-select: none;
+    color: var(--white);
+    font-size: var(--xsmall);
 
-    img {
-      width: ${getSpacing(3.8)};
-      height: ${getSpacing(3.8)};
-      object-fit: contain;
+    &:hover {
+      background-color: var(--background-2);
     }
 
-    .text {
+    .info {
+      width: calc(100% - ${getSpacing(noSelectedMark ? 0 : 5)});
       display: flex;
-      flex-direction: column;
-      overflow: hidden;
-      text-overflow: ellipsis;
+      align-items: center;
+      gap: ${getSpacing(1)};
+      text-transform: capitalize;
 
-      span {
-        padding-left: ${getSpacing(1)};
+      img {
+        width: ${getSpacing(3.8)};
+        height: ${getSpacing(3.8)};
+        object-fit: contain;
+      }
 
-        &.helper {
-          width: 100%;
-          text-overflow: ellipsis;
-          font-size: var(--xxsmall);
-          display: block;
-          overflow: hidden;
-          color: var(--grey-500);
-          margin-top: ${getSpacing(0.5)};
+      .text {
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+        text-overflow: ellipsis;
 
-          .vault-address {
-            display: flex;
-            align-items: center;
-            gap: ${getSpacing(0.5)};
+        span {
+          padding-left: ${getSpacing(1)};
+
+          &.helper {
+            width: 100%;
+            text-overflow: ellipsis;
+            font-size: var(--xxsmall);
+            display: block;
+            overflow: hidden;
+            color: var(--grey-500);
+            margin-top: ${getSpacing(0.5)};
+
+            .vault-address {
+              display: flex;
+              align-items: center;
+              gap: ${getSpacing(0.5)};
+            }
           }
         }
       }
     }
-  }
 
-  input {
-    display: none;
-  }
-`;
+    input {
+      display: none;
+    }
+  `
+);
