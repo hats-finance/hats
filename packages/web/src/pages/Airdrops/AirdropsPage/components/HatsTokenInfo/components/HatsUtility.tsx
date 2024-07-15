dadb860c9a21afeb21b96807fdbc9a05f9617dfa
Carlos Fontes <fonstack@gmail.com>
2024-07-15 16:28:06 +0100

    chore: new images and UI changes

--- packages/web/src/pages/Airdrops/AirdropsPage/components/HatsTokenInfo/components/HatsUtility.tsx
+++ packages/web/src/pages/Airdrops/AirdropsPage/components/HatsTokenInfo/components/HatsUtility.tsx
@@ -1,7 +1,5 @@
 import { useTranslation } from "react-i18next";
 
-const UTILITIES = [1, 2, 3, 4, 5, 6];
-
 export const HatsUtility = () => {
   const { t } = useTranslation();
 
@@ -11,8 +9,20 @@ export const HatsUtility = () => {
       <h1 className="mt-4">{t("Airdrop.unlockExclusiveBenefits")}</h1>
       <p className="text-info mt-2">{t("Airdrop.unlockBenefitsDescription")}</p>
 
-      <div className="utilities">
-        {UTILITIES.map((index) => {
+      <div className="utilities mb-4">
+        {[1, 2, 3].map((index) => {
+          return (
+            <div key={index} className="utility">
+              <img src={require(`../assets/utility-${index}.png`)} alt={`hats utility ${index}`} />
+              <h3>{t(`Airdrop.hatsUtility-${index}`)}</h3>
+              <p>{t(`Airdrop.hatsUtilityContent-${index}`)}</p>
+            </div>
+          );
+        })}
+      </div>
+      <h3>{t("Airdrop.forSecurityResearchers")}:</h3>
+      <div className="utilities pt-4">
+        {[4, 5, 6].map((index) => {
           return (
             <div key={index} className="utility">
               <img src={require(`../assets/utility-${index}.png`)} alt={`hats utility ${index}`} />
