777c5997c8aa76159cfa0ac025070962a4d01907
Carlos Fontes <fonstack@gmail.com>
2024-07-24 00:02:03 +0100

    chore: not redirecting back if no wallet connectec on `MyWallet` page

--- packages/web/src/pages/MyWallet/MyWalletPage/MyWalletPage.tsx
+++ packages/web/src/pages/MyWallet/MyWalletPage/MyWalletPage.tsx
@@ -1,7 +1,6 @@
-import { RoutePaths } from "navigation";
+import { Alert, WalletButton } from "components";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Navigate } from "react-router-dom";
 import { shortenAddress } from "utils/addresses.utils";
 import { useAccount } from "wagmi";
 import { DaoOverview } from "./Sections/DaoOverview/DaoOverview";
@@ -16,7 +15,18 @@ export const MyWalletPage = () => {
   const { address: account } = useAccount();
   // const { data: createdProfile, isLoading: isLoadingProfile } = useProfileByAddress(account);
 
-  if (!account) return <Navigate to={RoutePaths.vaults} replace={true} />;
+  if (!account)
+    return (
+      <>
+        <Alert className="mb-3" type="warning">
+          <span>{t("youNeedToConnectToAWallet")}</span>
+        </Alert>
+
+        <div className="mt-5">
+          <WalletButton expanded />
+        </div>
+      </>
+    );
 
   const sections = {
     dao: <DaoOverview />,
