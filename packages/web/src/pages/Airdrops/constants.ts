473bccc65b8f5b11a2cf3571956ee76cc3f19f33
Carlos Fontes <fonstack@gmail.com>
2024-07-24 20:29:53 +0100

    chore: updated vault to deposit

--- packages/web/src/pages/Airdrops/constants.ts
+++ packages/web/src/pages/Airdrops/constants.ts
@@ -1,7 +1,7 @@
-import { mainnet, sepolia } from "wagmi";
+import { arbitrum } from "wagmi/chains";
 
 export const VAULT_TO_DEPOSIT = {
-  address: "0x27d0a7eecb3ed321f86b90a79e24d1735e07e909",
-  chain: sepolia,
+  address: "0x1025b2248cb6aeaf93c7e4d10b19f90f5b4ea090",
+  chain: arbitrum,
 };
 // export const VAULT_TO_DEPOSIT = "0xfb277b477e9e14da8d3d4e06360b9b772825fef8"; // with APY
