835da04c3fbff125f83823d243fe6d678189690f
Carlos Fontes <fonstack@gmail.com>
2025-02-24 23:02:35 +0000

    chore: removed unused code

--- packages/web/src/layout/BasicLayout/BasicLayout.tsx
+++ packages/web/src/layout/BasicLayout/BasicLayout.tsx
@@ -1,16 +1,14 @@
 import { changeScreenSize } from "actions/index";
-import { AirdropModalAlert, CookiesBanner, Header, Modal, Sidebar } from "components";
+import { CookiesBanner, Header, Sidebar } from "components";
 import { LocalStorage, SMALL_SCREEN_BREAKPOINT, ScreenSize } from "constants/constants";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
-import { isAirdropEnabled } from "settings";
 import { AppContent, AppLayout, ContentWrapper, StyledApp } from "./styles";
 
 const BasicLayout = (): JSX.Element => {
   const dispatch = useDispatch();
   const [acceptedCookies, setAcceptedCookies] = useState(localStorage.getItem(LocalStorage.Cookies));
-  const [airdropModalSeen, setAirdropModalSeen] = useState(localStorage.getItem(LocalStorage.AirdropModalSeen));
 
   const screenSize = window.matchMedia(`(min-width: ${SMALL_SCREEN_BREAKPOINT})`);
   screenSize.addEventListener("change", (screenSize) => {
