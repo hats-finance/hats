a996bf3bd5e070689b7409b3019b3811a3146f86
Shay Zluf <thezluf@gmail.com>
2025-12-12 15:09:47 +0530

    fix withdrawal issue

--- packages/web/src/contracts/read/multicalls/DepositDataMulticall.ts
+++ packages/web/src/contracts/read/multicalls/DepositDataMulticall.ts
@@ -17,25 +17,33 @@ export class DepositWithdrawDataMulticall {
     const contractAddress = selectedVault.version === "v1" ? selectedVault.master.address : selectedVault.id;
     const vaultToken = selectedVault.stakingToken as `0x${string}`;
 
+    // Build contracts array, filtering out undefined values
+    const depositTierContract = DepositTierContract.contractInfo(selectedVault, account);
+    const contracts = [
+      TokenAllowanceContract.contractInfo(vaultToken, account, contractAddress, selectedVault.chainId),
+      UserSharesPerVaultContract.contractInfo(selectedVault, account),
+      TotalSharesPerVaultContract.contractInfo(selectedVault),
+      depositTierContract,
+    ].filter((contract) => contract !== undefined);
+
     const { data: res, isError } = useContractReads({
       enabled: isTabFocused,
       scopeKey: "hats",
       watch: isTabFocused,
-      contracts: [
-        TokenAllowanceContract.contractInfo(vaultToken, account, contractAddress, selectedVault.chainId),
-        UserSharesPerVaultContract.contractInfo(selectedVault, account),
-        TotalSharesPerVaultContract.contractInfo(selectedVault),
-        DepositTierContract.contractInfo(selectedVault, account),
-      ],
+      contracts,
     });
     const data = res as any;
+
+    // Determine the index of tierFromShares in the response
+    // It's always at index 3 if depositTierContract exists, otherwise undefined
+    const tierFromSharesIndex = depositTierContract ? 3 : undefined;
 
     return !isError && data
       ? {
           tokenAllowance: TokenAllowanceContract.mapResponseToData({ data: data?.[0] }),
           userSharesAvailable: UserSharesPerVaultContract.mapResponseToData({ data: data?.[1] }, selectedVault),
           totalSharesAvailable: TotalSharesPerVaultContract.mapResponseToData({ data: data?.[2] }, selectedVault),
-          tierFromShares: DepositTierContract.mapResponseToData({ data: data?.[3] }),
+          tierFromShares: tierFromSharesIndex !== undefined ? DepositTierContract.mapResponseToData({ data: data?.[tierFromSharesIndex] }) : undefined,
         }
       : {};
   };
